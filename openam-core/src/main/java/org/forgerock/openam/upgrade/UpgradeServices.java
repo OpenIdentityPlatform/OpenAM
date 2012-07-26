5970c0c5bc78ab3ed84a9a21f87ad1b6b00782f4
jeff.schenk <jeff.schenk@forgerock.com>
2012-07-26 17:41:39 +0000

    Remove unused imports.

--- openam-core/src/main/java/org/forgerock/openam/upgrade/UpgradeServices.java
+++ openam-core/src/main/java/org/forgerock/openam/upgrade/UpgradeServices.java
@@ -51,16 +51,16 @@ import com.sun.identity.shared.xml.XMLUtils;
 import com.sun.identity.sm.SMSException;
 import com.sun.identity.sm.ServiceManager;
 import com.sun.identity.sm.ServiceSchemaModifications;
-import java.io.BufferedReader;
+
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.net.URL;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
