c9fc48b445eb55918ed1aae9d1f8a7733364a5b9
jeff.schenk <jeff.schenk@forgerock.com>
2012-07-30 16:59:12 +0000

    Changed Serializable Long to 1010.

--- openam-core/src/main/java/org/forgerock/openam/session/model/AMRootEntity.java
+++ openam-core/src/main/java/org/forgerock/openam/session/model/AMRootEntity.java
@@ -13,7 +13,7 @@ import java.io.Serializable;
  *
  */
 public abstract class AMRootEntity implements Serializable {
-    private static final long serialVersionUID = 100100L;
+    private static final long serialVersionUID = 1010L;
 
 
 
