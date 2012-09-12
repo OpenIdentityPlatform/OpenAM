2aae7e94be9f6416f608cf440caa9d20f78e415c
jeff.schenk <jeff.schenk@forgerock.com>
2012-09-12 20:06:07 +0000

    COMMWEB-82 Fix SCM Statement in main pom to point to trunk instead of old branch.

--- src/main/jenkins/prepare_downloads.sh
+++ src/main/jenkins/prepare_downloads.sh
@@ -7,12 +7,12 @@ OPENAM_VERSION="10.1.0-SNAPSHOT";
 mv "$WORKSPACE"/openam-server/target/openam-server-${OPENAM_VERSION}.war \
     "$WORKSPACE"/openam-server/target/openam-server-${OPENAM_VERSION}_`date "+%Y%m%d"`.war
 
-mv "$WORKSPACE"/openam-distribution/openam-distribution-ssoadmintools/target/openam-distribution-ssoAdminTools-${OPENAM_VERSION}.zip \
-   "$WORKSPACE"/openam-distribution/openam-distribution-ssoadmintools/target/openam-distribution-ssoAdminTools-${OPENAM_VERSION}_`date "+%Y%m%d"`.zip
+mv openam-distribution/openam-distribution-ssoadmintools/target/openam-distribution-ssoAdminTools-${OPENAM_VERSION}.zip \
+   openam-distribution/openam-distribution-ssoadmintools/target/openam-distribution-ssoAdminTools-${OPENAM_VERSION}_`date "+%Y%m%d"`.zip
 
-mv "$WORKSPACE"/openam-distribution/openam-distribution-ssoconfiguratortools/target/openam-distribution-ssoconfiguratortools-${OPENAM_VERSION}.zip \
-   "$WORKSPACE"/openam-distribution/openam-distribution-ssoconfiguratortools/target/openam-distribution-ssoconfiguratortools-${OPENAM_VERSION}_`date "+%Y%m%d"`.zip
+mv openam-distribution/openam-distribution-ssoconfiguratortools/target/openam-distribution-ssoconfiguratortools-${OPENAM_VERSION}.zip \
+   openam-distribution/openam-distribution-ssoconfiguratortools/target/openam-distribution-ssoconfiguratortools-${OPENAM_VERSION}_`date "+%Y%m%d"`.zip
 
-mv "$WORKSPACE"/openam-distribution/openam-distribution-kit/target/openam-distribution-kit--${OPENAM_VERSION}.zip \
-   "$WORKSPACE"/openam-distribution/openam-distribution-kit/target/openam-distribution-kit-${OPENAM_VERSION}_`date "+%Y%m%d"`.zip
+mv openam-distribution/openam-distribution-kit/target/openam-distribution-kit--${OPENAM_VERSION}.zip \
+   openam-distribution/openam-distribution-kit/target/openam-distribution-kit-${OPENAM_VERSION}_`date "+%Y%m%d"`.zip
 
