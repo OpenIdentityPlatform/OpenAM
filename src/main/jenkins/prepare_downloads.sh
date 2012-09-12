f131ef75fa02edc15fdbfbc2a411af571378bbb8
jeff.schenk <jeff.schenk@forgerock.com>
2012-09-12 18:25:52 +0000

    COMMWEB-82 Fix to properly setup and prepare files for nightly downloads.  Additional changes on Jenkins' OpenAM Maven Job.

---
+++ src/main/jenkins/prepare_downloads.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Script used within Jenkins Build Process for OpenAM
+# Prepare Artifacts for versioning on Download Site.
+#
+OPENAM_VERSION="10.1.0-SNAPSHOT";
+mv "$WORKSPACE"/openam-server/target/openam-server-${OPENAM_VERSION}.war \
+    "$WORKSPACE"/openam-server/target/openam-server-${OPENAM_VERSION}_`date "+%Y%m%d"`.war
+
+mv "$WORKSPACE"/openam-distribution/openam-distribution-ssoadmintools/target/openam-distribution-ssoAdminTools-${OPENAM_VERSION}.zip \
+   "$WORKSPACE"/openam-distribution/openam-distribution-ssoadmintools/target/openam-distribution-ssoAdminTools-${OPENAM_VERSION}_`date "+%Y%m%d"`.zip
+
+mv "$WORKSPACE"/openam-distribution/openam-distribution-ssoconfiguratortools/target/openam-distribution-ssoconfiguratortools-${OPENAM_VERSION}.zip \
+   "$WORKSPACE"/openam-distribution/openam-distribution-ssoconfiguratortools/target/openam-distribution-ssoconfiguratortools-${OPENAM_VERSION}_`date "+%Y%m%d"`.zip
+
+mv "$WORKSPACE"/openam-distribution/openam-distribution-kit/target/openam-distribution-kit--${OPENAM_VERSION}.zip \
+   "$WORKSPACE"/openam-distribution/openam-distribution-kit/target/openam-distribution-kit-${OPENAM_VERSION}_`date "+%Y%m%d"`.zip
+
