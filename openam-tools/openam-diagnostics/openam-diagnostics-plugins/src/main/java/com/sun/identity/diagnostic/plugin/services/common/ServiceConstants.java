f8101ebe31806b47ebf96a18a7cd3131b872b1bf
Peter Major <majorpetya@gmail.com>
2012-09-15 23:45:31 +0000

    Additional fix for OPENAM-1658

--- openam-tools/openam-diagnostics/openam-diagnostics-plugins/src/main/java/com/sun/identity/diagnostic/plugin/services/common/ServiceConstants.java
+++ openam-tools/openam-diagnostics/openam-diagnostics-plugins/src/main/java/com/sun/identity/diagnostic/plugin/services/common/ServiceConstants.java
@@ -25,80 +25,80 @@
  * $Id: ServiceConstants.java,v 1.2 2008/12/16 17:54:07 ak138937 Exp $
  *
  */
-
-/**
+ 
+/*
  * Portions Copyrighted 2011-2012 ForgeRock Inc
  */
 package com.sun.identity.diagnostic.plugin.services.common;
-
+ 
 import com.sun.identity.shared.Constants;
-
+ 
 /**
  * This interface contains the property names used by the 
  * services
  */
 public interface ServiceConstants extends Constants {
-    /*
+   /*
      * Property strings for bootrap file parsing.
      */
-    String PROTOCOL = "protocol";
-    String SERVER_INSTANCE = "serverinstance";
-    String FF_BASE_DIR = "ffbasedir";
-    String BASE_DIR = "basedir";
-    String DS_HOST = "dshost";
-    String DS_PORT = "dsport";
-    String DS_PWD = "dspwd";
-    String DS_MGR = "dsmgr";
-    String DS_BASE_DN = "dsbasedn";
-    String PWD = "pwd";
-    String EMBEDDED_DS = "embeddedds";
-    String PROT_FILE = "file";
-    String PROT_LDAP = "ldap";
-    String PROTOCOL_FILE = "file://";
-    String PROTOCOL_LDAP = "ldap://";
-    String PROTOCOL_LDAPS = "ldaps://";
-    String DS_PROTOCOL = "dsprotocol";
-    String LOCATION = "location";
-    String BOOTSTRAP = "bootstrap";
-    String BOOTPATH = "path";
-    String BASE_DIR_PATTERN = "%BASE_DIR%";
-    String DEP_URI_PATTERN = "%SERVER_URI%";
-    
-    /**
+   static final String PROTOCOL = "protocol";
+   static final String SERVER_INSTANCE = "serverinstance";
+   static final String FF_BASE_DIR = "ffbasedir";
+   static final String BASE_DIR = "basedir";
+   static final String DS_HOST = "dshost";
+   static final String DS_PORT = "dsport";
+   static final String DS_PWD = "dspwd";
+   static final String DS_MGR = "dsmgr";
+   static final String DS_BASE_DN = "dsbasedn";
+   static final String PWD = "pwd";
+   static final String EMBEDDED_DS = "embeddedds";
+   static final String PROT_FILE = "file";
+   static final String PROT_LDAP = "ldap";
+   static final String PROTOCOL_FILE = "file://";
+   static final String PROTOCOL_LDAP = "ldap://";
+   static final String PROTOCOL_LDAPS = "ldaps://";
+   static final String DS_PROTOCOL = "dsprotocol";
+   static final String LOCATION = "location";
+   static final String BOOTSTRAP = "bootstrap";
+   static final String BOOTPATH = "path";
+   static final String BASE_DIR_PATTERN = "%BASE_DIR%";
+   static final String DEP_URI_PATTERN = "%SERVER_URI%";
+   
+   /**
      * Session failover related properties to be checked in Server 
      * configuration service
      */
-    final String amSessionService = "iPlanetAMSessionService";
-    final String IS_SFO_ENABLED = "iplanet-am-session-sfo-enabled";
-    final String SESSION_STORE_USERNAME =
-        "iplanet-am-session-store-username";
-    final String SESSION_STORE_PASSWORD =
-        "iplanet-am-session-store-password";
-    final String CONNECT_MAX_WAIT_TIME =
-        "iplanet-am-session-store-cpl-max-wait-time";
-    final String JDBC_DRIVER_CLASS =
-        "iplanet-am-session-JDBC-driver-Impl-classname";
-    static final String IPLANET_AM_SESSION_REPOSITORY_URL =
-         "iplanet-am-session-repository-url";
-    static final String MIN_POOL_SIZE =
-        "iplanet-am-session-min-pool-size";
-    final String MAX_POOL_SIZE =
-        "iplanet-am-session-max-pool-size";
-    static final String IS_SFO_ENABLED =
-            "iplanet-am-session-sfo-enabled";
-
-    /**
+   static final String amSessionService = "iPlanetAMSessionService";
+ 
+   static final String SESSION_STORE_USERNAME =
+       "iplanet-am-session-store-username";
+   static final String SESSION_STORE_PASSWORD =
+       "iplanet-am-session-store-password";
+   static final String CONNECT_MAX_WAIT_TIME =
+       "iplanet-am-session-store-cpl-max-wait-time";
+   static final String JDBC_DRIVER_CLASS =
+       "iplanet-am-session-JDBC-driver-Impl-classname";
+   static final String IPLANET_AM_SESSION_REPOSITORY_URL =
+        "iplanet-am-session-repository-url";
+   static final String MIN_POOL_SIZE =
+       "iplanet-am-session-min-pool-size";
+   static final String MAX_POOL_SIZE =
+       "iplanet-am-session-max-pool-size";
+   static final String IS_SFO_ENABLED =
+           "iplanet-am-session-sfo-enabled";
+ 
+   /**
      * General Server  related properties
      */
-    final String CONFIG_PATH = "com.iplanet.services.configpath";
-
-    /**
+   static final String CONFIG_PATH = "com.iplanet.services.configpath";
+ 
+   /**
      * Single line format string 
      */
-     final String SMALL_LINE_SEP_1 = "---------------------------------------";
-
-    /**
+   static final String SMALL_LINE_SEP_1 = "---------------------------------------";
+ 
+   /**
      * Double line format string 
      */
-     final String PARA_SEP_1 = "==============================";
+   static final String PARA_SEP_1 = "==============================";
 }
