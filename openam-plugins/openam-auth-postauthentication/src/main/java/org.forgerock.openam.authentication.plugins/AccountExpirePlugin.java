16fcc0dffff54ee03f87f26f2da92bf58107a5f6
Allan Foster <allan.foster@forgerock.com>
2012-11-15 23:48:51 +0000

    AME-219 -  CR-889  - fixed module nae in Pom

--- openam-plugins/openam-auth-postauthentication/src/main/java/org.forgerock.openam.authentication.plugins/AccountExpirePlugin.java
+++ openam-plugins/openam-auth-postauthentication/src/main/java/org.forgerock.openam.authentication.plugins/AccountExpirePlugin.java
@@ -1,7 +1,7 @@
 /**
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2011 ForgeRock AS. All Rights Reserved
+ * Copyright (c) 2012 ForgeRock Inc. All Rights Reserved
  *
  * The contents of this file are subject to the terms
  * of the Common Development and Distribution License
