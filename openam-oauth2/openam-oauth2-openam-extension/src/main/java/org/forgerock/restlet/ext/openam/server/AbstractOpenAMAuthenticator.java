338572e0f7937fbb8d1eb654bffb26b450f66f13
Jason Lemay <jason.lemay@forgerock.com>
2012-12-17 01:10:31 +0000

    CR-1123 OPENAM-2039 FIX

--- openam-oauth2/openam-oauth2-openam-extension/src/main/java/org/forgerock/restlet/ext/openam/server/AbstractOpenAMAuthenticator.java
+++ openam-oauth2/openam-oauth2-openam-extension/src/main/java/org/forgerock/restlet/ext/openam/server/AbstractOpenAMAuthenticator.java
@@ -24,6 +24,8 @@
 
 package org.forgerock.restlet.ext.openam.server;
 
+import com.iplanet.services.naming.WebtopNaming;
+import com.sun.identity.federation.common.FSUtils;
 import com.sun.identity.shared.OAuth2Constants;
 import org.forgerock.restlet.ext.openam.OpenAMParameters;
 import org.forgerock.restlet.ext.openam.OpenAMUser;
@@ -33,6 +35,8 @@ import org.restlet.Request;
 import org.restlet.Response;
 import org.restlet.data.Reference;
 import org.restlet.data.Status;
+import org.restlet.ext.servlet.ServletUtils;
+import org.restlet.ext.servlet.internal.ServletCall;
 import org.restlet.resource.ResourceException;
 import org.restlet.routing.Redirector;
 import org.restlet.security.Authenticator;
@@ -43,6 +47,10 @@ import com.iplanet.sso.SSOToken;
 import com.sun.identity.idm.AMIdentity;
 import com.sun.identity.idm.IdRepoException;
 import com.sun.identity.idm.IdUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Used to authenticate to OpenAM and redirect to its login page.
@@ -136,21 +144,53 @@ public abstract class AbstractOpenAMAuthenticator extends Authenticator {
         if (OAuth2Utils.DEBUG.messageEnabled()){
             OAuth2Utils.DEBUG.message("Redirecting to OpenAM login page");
         }
-        Reference amserver = new Reference(openamServer);
+        HttpServletRequest httpRequest = ServletUtils.getRequest(request);
+        String authURL = null;
+        URI authURI = null;
+
+        authURL = getAuthURL(request);
+
+        /**
+         * TODO check for forward and do a forward vs a redirection See IDPSSOFederate.redirectAuthentication() for details
+        //check for forward or request
+        StringBuffer appliRootUrl = getAppliRootUrl(httpRequest);
+        boolean forward = false;
+        StringBuffer newURL;
+        // build newUrl to auth service and test if redirect or forward
+
+        if(FSUtils.isSameContainer(httpRequest, authURL)){
+            forward = true;
+            String relativePath = getRelativePath(authURL, appliRootUrl.
+                    toString());
+            newURL = new StringBuffer(relativePath);
+        } else {
+            // cannot forward so redirect
+            forward = false ;
+            newURL = new StringBuffer(authURL);
+        }
+        */
+
+        try {
+            authURI = new URI(authURL);
+        } catch (URISyntaxException e){
+            OAuth2Utils.DEBUG.error("Unable to construct authURI", e);
+            throw new ResourceException(Status.SERVER_ERROR_INTERNAL, e.getMessage(), e);
+        }
+        Reference amserver = new Reference(authURI);
         realm = OAuth2Utils.getRealm(request);
         moduleName = OAuth2Utils.getModuleName(request);
         serviceName = OAuth2Utils.getServiceName(request);
         locale = OAuth2Utils.getLocale(request);
 
-        if (null != realm) {
+        if (null != realm && !realm.isEmpty()) {
             amserver.addQueryParameter(OAuth2Constants.Custom.REALM, realm);
         }
-        if (null != locale){
+        if (null != locale && !locale.isEmpty()){
             amserver.addQueryParameter(OAuth2Constants.Custom.LOCALE, locale);
         }
-        if (null != moduleName) {
+        if (null != moduleName && !moduleName.isEmpty()) {
             amserver.addQueryParameter(OAuth2Constants.Custom.MODULE, moduleName);
-        } else if (null != serviceName) {
+        } else if (null != serviceName && !serviceName.isEmpty()) {
             amserver.addQueryParameter(OAuth2Constants.Custom.SERVICE, serviceName);
         }
 
@@ -159,5 +199,39 @@ public abstract class AbstractOpenAMAuthenticator extends Authenticator {
         Redirector redirector =
                 new Redirector(getContext(), amserver.toString(), Redirector.MODE_CLIENT_FOUND);
         redirector.handle(request, response);
+    }
+
+    private String getAuthURL(Request request){
+        HttpServletRequest httpRequest = ServletUtils.getRequest(request);
+        String uri = httpRequest.getRequestURI();
+        String deploymentURI = uri;
+        int firstSlashIndex = uri.indexOf("/");
+        int secondSlashIndex = uri.indexOf("/", firstSlashIndex + 1);
+        if (secondSlashIndex != -1) {
+            deploymentURI = uri.substring(0, secondSlashIndex);
+        }
+        StringBuffer sb = new StringBuffer(100);
+        sb.append(httpRequest.getScheme()).append("://")
+        .append(httpRequest.getServerName()).append(":")
+        .append(httpRequest.getServerPort())
+                .append(deploymentURI)
+        .append("/UI/Login");
+        return sb.toString();
+    }
+
+    private static StringBuffer getAppliRootUrl(HttpServletRequest request) {
+        StringBuffer result = new StringBuffer();
+        String scheme = request.getScheme();             // http
+        String serverName = request.getServerName();     // hostname.com
+        int serverPort = request.getServerPort();        // 80
+        String contextPath = request.getContextPath();   // /mywebapp
+        result.append(scheme).append("://").append(serverName).append(":").
+                append(serverPort);
+        result.append(contextPath);
+        return result ;
+    }
+
+    private static String getRelativePath(String absUrl, String appliRootUrl) {
+        return absUrl.substring(appliRootUrl.length(), absUrl.length());
     }
 }
