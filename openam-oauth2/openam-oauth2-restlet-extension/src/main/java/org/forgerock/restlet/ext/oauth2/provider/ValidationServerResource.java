ccce0e92768083393478fcb1c9570283d7cbebdd
Jason Lemay <jason.lemay@forgerock.com>
2013-04-30 22:42:56 +0000

    AME-1713 commit missed file

--- openam-oauth2/openam-oauth2-restlet-extension/src/main/java/org/forgerock/restlet/ext/oauth2/provider/ValidationServerResource.java
+++ openam-oauth2/openam-oauth2-restlet-extension/src/main/java/org/forgerock/restlet/ext/oauth2/provider/ValidationServerResource.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2012 ForgeRock Inc. All rights reserved.
+ * Copyright (c) 2012-2013 ForgeRock Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms
  * of the Common Development and Distribution License
@@ -19,7 +19,7 @@
  * If applicable, add the following below the CDDL Header,
  * with the fields enclosed by brackets [] replaced by
  * your own identifying information:
- * "Portions Copyrighted [2012] [ForgeRock Inc]"
+ * "Portions Copyrighted [year] [name of copyright owner]"
  */
 
 package org.forgerock.restlet.ext.oauth2.provider;
@@ -37,21 +37,19 @@ import com.sun.identity.security.AdminTokenAction;
 import com.sun.identity.sm.ServiceConfig;
 import com.sun.identity.sm.ServiceConfigManager;
 import com.sun.identity.shared.OAuth2Constants;
+import org.forgerock.openam.oauth2.model.CoreToken;
+import org.forgerock.openam.oauth2.model.BearerToken;
 import org.forgerock.openam.oauth2.provider.OAuth2TokenStore;
 import org.forgerock.openam.oauth2.utils.OAuth2Utils;
 import org.forgerock.openam.oauth2.exceptions.OAuthProblemException;
+import org.forgerock.restlet.ext.oauth2.consumer.AccessTokenExtractor;
 import org.forgerock.restlet.ext.oauth2.consumer.AccessTokenValidator;
-import org.forgerock.restlet.ext.oauth2.consumer.BearerAuthenticatorHelper;
-import org.forgerock.restlet.ext.oauth2.consumer.BearerToken;
-import org.forgerock.openam.oauth2.model.AccessToken;
+import org.forgerock.restlet.ext.oauth2.consumer.BearerTokenExtractor;
+import org.restlet.Client;
 import org.restlet.Context;
 import org.restlet.Request;
 import org.restlet.Response;
-import org.restlet.data.CacheDirective;
-import org.restlet.data.Form;
-import org.restlet.data.Method;
-import org.restlet.data.Reference;
-import org.restlet.data.Status;
+import org.restlet.data.*;
 import org.restlet.ext.jackson.JacksonRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.ClientResource;
@@ -63,26 +61,6 @@ import org.forgerock.openam.oauth2.provider.Scope;
 /**
  * Validates the token and returns to the subject the tokeninfo and scope evaluation if it is used.
  * <p/>
- * 
- * <pre>
- * 
- * 
- * param parameters
- *  {
- *      "access_token":"1/fFBGRNJru1FQd44AzqT3Zg"
- *  }
- * 
- * return
- *  {
- *      "audience":"client_id",
- *      "user_id":"123456789",
- *      "scope":[
- *                  "https://read.openam.forgerock.org"
- *              ],
- *      "expires_in":436
- *  }
- * 
- * </pre>
  * 
  * @author Laszlo Hordos
  */
@@ -97,6 +75,12 @@ public class ValidationServerResource extends ServerResource implements
     }
 
     public ValidationServerResource(Context context, Reference validationServerRef) {
+        tokenStore = OAuth2Utils.getTokenStore(context);
+        if (null == tokenStore) {
+            OAuth2Utils.DEBUG.error("ValidationServerResource::Unable to get token store");
+            throw new ResourceException(Status.SERVER_ERROR_INTERNAL,
+                    "Missing required context attribute: " + OAuth2TokenStore.class.getName());
+        }
         this.validationServerRef = validationServerRef;
         init(context, null, null);
     }
@@ -119,7 +103,7 @@ public class ValidationServerResource extends ServerResource implements
         }
     }
 
-    @Get("json")
+    @Get()
     public Representation validate() throws ResourceException {
         if (OAuth2Utils.DEBUG.messageEnabled()){
             OAuth2Utils.DEBUG.message("ValidationServerResource::In Validator resource");
@@ -131,7 +115,8 @@ public class ValidationServerResource extends ServerResource implements
 
         try {
             Form call = getQuery();
-            String token = call.getFirstValue(OAuth2Constants.Params.ACCESS_TOKEN);
+            Map<String, Object> tokenMap = AccessTokenExtractor.extractToken(getRequest());
+            String token = (String) tokenMap.get("access_token");
 
             if (null == token) {
                 OAuth2Utils.DEBUG.error("ValidationServerResource::Missing access token in request");
@@ -139,7 +124,7 @@ public class ValidationServerResource extends ServerResource implements
                         OAuthProblemException.OAuthError.INVALID_REQUEST.handle(getRequest(),
                                 "Missing access_token");
             } else {
-                AccessToken t = tokenStore.readAccessToken(token);
+                CoreToken t = tokenStore.readAccessToken(token);
 
                 if (t == null) {
                     OAuth2Utils.DEBUG.error("ValidationServerResource::Unable to read token from token store for id: " + token);
@@ -192,43 +177,24 @@ public class ValidationServerResource extends ServerResource implements
     @Override
     public BearerToken verify(BearerToken token) throws OAuthProblemException {
         Reference reference = new Reference(validationServerRef);
-        reference.addQueryParameter(OAuth2Constants.Params.ACCESS_TOKEN, token.getAccessToken());
-        ClientResource clientResource = new ClientResource(getContext(), reference);
+        reference.addQueryParameter(OAuth2Constants.Params.ACCESS_TOKEN, token.getTokenID());
+        Client client = new Client(new Context(), Protocol.HTTP);
+        ClientResource clientResource = new ClientResource(reference.toUri());
+        clientResource.setNext(client);
+        clientResource.get();
         try {
-            Request request = new Request(Method.GET, reference, null);
-
             // Actually handle the call
-            Response response = clientResource.handleOutbound(request);
+            Response response = clientResource.getResponse();
 
             // Throws OAuthProblemException
-            Map remoteToken = BearerAuthenticatorHelper.extractToken(response);
+            Map remoteToken = BearerTokenExtractor.extractToken(response);
 
-            Object o = remoteToken.get(OAuth2Constants.Token.OAUTH_EXPIRES_IN);
-            Number expires_in = token.getExpiresIn();
-            if (o instanceof Number) {
-                expires_in = (Number) o;
+            Object o = remoteToken.get(OAuth2Constants.Token.OAUTH_ACCESS_TOKEN);
+            if (o != null){
+                return (BearerToken) tokenStore.readAccessToken(o.toString());
             }
 
-            o = remoteToken.get(OAuth2Constants.Custom.AUDIENCE);
-            String client_id = null;
-            if (o instanceof String) {
-                client_id = (String) o;
-            }
-
-            o = remoteToken.get(OAuth2Constants.Custom.USER_ID);
-            String username = null;
-            if (o instanceof String) {
-                username = (String) o;
-            }
-
-            o = remoteToken.get(OAuth2Constants.Params.SCOPE);
-            Set<String> scope = null;
-            if (o instanceof Collection) {
-                scope =
-                        Collections.unmodifiableSet(new HashSet<String>(
-                                (Collection<? extends String>) o));
-            }
-            return new BearerToken(token, expires_in, client_id, username, scope);
+            return null;
         } catch (OAuthProblemException e) {
             OAuth2Utils.DEBUG.error("ValidationServerResource::Error occurred during token verify", e);
             throw e;
