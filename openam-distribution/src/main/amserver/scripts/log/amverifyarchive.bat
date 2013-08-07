99b27fb20dcf0c0e55c26f115c7ef4cb657467aa
Travis Papp <travis.papp@forgerock.com>
2013-08-07 18:07:54 +0000

    CR-2119 OPENAM-2718 Replaced the hard coded opendj server version with the dynamic from the pom

--- openam-distribution/src/main/amserver/scripts/log/amverifyarchive.bat
+++ openam-distribution/src/main/amserver/scripts/log/amverifyarchive.bat
@@ -41,7 +41,7 @@ goto WHILE
 set CLASSPATH="@CONFIG_DIR@"
 set CLASSPATH="%CLASSPATH%;lib/forgerock-util-${commons.forgerock-util.version}.jar"
 set CLASSPATH="%CLASSPATH%;lib/openam-distribution-amadmsetup-${project.version}.jar"
-set CLASSPATH="%CLASSPATH%;lib/opendj-server-2.4.6.jar"
+set CLASSPATH="%CLASSPATH%;lib/opendj-server-${opendj.server.version}.jar"
 set CLASSPATH="%CLASSPATH%;lib/mail-1.4.5.jar"
 set CLASSPATH="%CLASSPATH%;lib/j2ee-2007-18-10.jar"
 set CLASSPATH="%CLASSPATH%;lib/webservices-api-2009-14-01.jar"
