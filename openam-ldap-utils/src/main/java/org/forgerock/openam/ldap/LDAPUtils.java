15d81cfe2432d3a94ac50d305ff93e62892b512d
Peter Major <majorpetya@gmail.com>
2013-09-27 13:03:10 +0000

    Fix for AME-2484 - reviewed by Matt Updating OpenDJ LDAP SDK version to 2.6.2

--- openam-ldap-utils/src/main/java/org/forgerock/openam/ldap/LDAPUtils.java
+++ openam-ldap-utils/src/main/java/org/forgerock/openam/ldap/LDAPUtils.java
@@ -64,6 +64,7 @@ public class LDAPUtils {
     private static final String LDAP_SCOPE_SUB = "SCOPE_SUB";
     private static final Map<String, SearchScope> scopes;
     private static final Debug DEBUG = Debug.getInstance("LDAPUtils");
+    private static final int DEFAULT_HEARTBEAT_TIMEOUT_MS = 500;
 
     static {
         Map<String, SearchScope> mappings = new HashMap<String, SearchScope>(3);
@@ -218,7 +219,9 @@ public class LDAPUtils {
         }
         ConnectionFactory cf = new LDAPConnectionFactory(ldapurl.getHost(), ldapurl.getPort(), ldapOptions);
         if (heartBeatInterval > 0) {
-            cf = Connections.newHeartBeatConnectionFactory(cf, heartBeatInterval, TimeUnit.valueOf(heartBeatTimeUnit));
+            TimeUnit unit = TimeUnit.valueOf(heartBeatTimeUnit.toUpperCase());
+            cf = Connections.newHeartBeatConnectionFactory(cf, unit.toMillis(heartBeatInterval),
+                    DEFAULT_HEARTBEAT_TIMEOUT_MS, TimeUnit.MILLISECONDS);
         }
         if (username != null) {
             cf = Connections.newAuthenticatedConnectionFactory(cf, Requests.newSimpleBindRequest(username, password));
