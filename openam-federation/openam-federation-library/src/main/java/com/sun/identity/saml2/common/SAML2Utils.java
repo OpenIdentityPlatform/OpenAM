343309f2ff8d579e537c026000888a55d5fffc6b
Peter Major <majorpetya@gmail.com>
2014-03-02 23:19:53 +0000

    Fix for OPENAM-3707 Adding extra emptiness check to ensure we don't try to retrieve affiliationDescriptor for "".

--- openam-federation/openam-federation-library/src/main/java/com/sun/identity/saml2/common/SAML2Utils.java
+++ openam-federation/openam-federation-library/src/main/java/com/sun/identity/saml2/common/SAML2Utils.java
@@ -1092,7 +1092,7 @@ public class SAML2Utils extends SAML2SDKUtils {
 
         NameIDInfoKey infoKey = null;
         String affiliationID = nameID.getSPNameQualifier();
-        if (affiliationID != null) {
+        if (affiliationID != null && !affiliationID.isEmpty()) {
             AffiliationDescriptorType affiDesc =
                 saml2MetaManager.getAffiliationDescriptor(realm, affiliationID);
             if (affiDesc == null) {
