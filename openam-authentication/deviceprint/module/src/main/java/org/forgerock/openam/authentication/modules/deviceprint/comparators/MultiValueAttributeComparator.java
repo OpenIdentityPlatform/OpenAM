6d5b0856893652294730a0d98289feb12237b074
Kohei Tamura <ktamura.biz.80@gmail.com>
2014-04-01 01:29:37 +0000

    Fix for OPENAM-3783, review CR-3254: Device print check of installed plugins and fonts does not work

--- openam-authentication/deviceprint/module/src/main/java/org/forgerock/openam/authentication/modules/deviceprint/comparators/MultiValueAttributeComparator.java
+++ openam-authentication/deviceprint/module/src/main/java/org/forgerock/openam/authentication/modules/deviceprint/comparators/MultiValueAttributeComparator.java
@@ -25,7 +25,8 @@
  */
 /*
  * Portions Copyrighted 2013 Syntegrity.
- * Portions Copyrighted 2013 ForgeRock Inc.
+ * Portions Copyrighted 2013-2014 ForgeRock Inc.
+ * Portions Copyrighted 2014 Nomura Research Institute, Ltd.
  */
 
 package org.forgerock.openam.authentication.modules.deviceprint.comparators;
@@ -34,8 +35,6 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.forgerock.openam.authentication.modules.deviceprint.DevicePrintModule;
 
 import com.sun.identity.shared.debug.Debug;
 
@@ -50,7 +49,7 @@ public class MultiValueAttributeComparator {
 
 	private static final BigDecimal HUNDRED = BigDecimal.valueOf(100);
 
-    private static final String DELIMITER = ",";
+    private static final String DELIMITER = ";";
 
 	/**
 	 * Splits both attributes using delimiter, trims every value and compares collections of values.
@@ -60,7 +59,7 @@ public class MultiValueAttributeComparator {
      * percentage of difference is less or equal maxToleratedPercentageToMarkAsDifferent.
      *
      * If yes then returns zero-result with additional info, else returns penaltyPoints-result.
-	 * 
+	 *
 	 * @param currentAttribute The current value.
 	 * @param storedAttribute The stored value.
      * @param maxToleratedNumberOfDifferences The max number of differences in the values, before the penalty points
@@ -73,23 +72,23 @@ public class MultiValueAttributeComparator {
 	public ComparisonResult compare(String currentAttribute, String storedAttribute,
             Integer maxToleratedPercentageToMarkAsDifferent, Integer maxToleratedNumberOfDifferences,
             long penaltyPoints) {
-		
+
 		List<String> currentAttributes = convertAttributesToList(currentAttribute);
 		List<String> storedAttributes = convertAttributesToList(storedAttribute);
-		
+
 		if (storedAttribute == null && currentAttribute != null && currentAttributes.size() == 0) {
 			return new ComparisonResult(true);
 		}
 
 		BigDecimal maxToleratedDifferences = BigDecimal.valueOf(maxToleratedNumberOfDifferences);
 		BigDecimal maxToleratedPercentage = BigDecimal.valueOf(maxToleratedPercentageToMarkAsDifferent);
-		
+
 		BigDecimal maxNumberOfElements = BigDecimal.valueOf(Math.max(currentAttributes.size(),
                 storedAttributes.size()));
 		BigDecimal numberOfTheSameElements = getNumberOfSameElements(currentAttributes,storedAttributes);
 		BigDecimal numberOfDifferences = getNumberOfDifferences(numberOfTheSameElements,maxNumberOfElements);
 		BigDecimal percentageOfDifferences = getPercentageOfDifferences(numberOfDifferences, maxNumberOfElements);
-		
+
 
 		if (DEBUG.messageEnabled()) {
 			DEBUG.message(numberOfTheSameElements + " of " + maxNumberOfElements + " are same");
@@ -101,7 +100,7 @@ public class MultiValueAttributeComparator {
 			}
 			return ComparisonResult.ZERO_PENALTY_POINTS;
 		}
-		
+
 		if (numberOfTheSameElements.equals(maxNumberOfElements)) {
 			if (DEBUG.messageEnabled()) {
 				DEBUG.message("Ignored because all attributes are same");
@@ -111,7 +110,7 @@ public class MultiValueAttributeComparator {
 
 		if (numberOfDifferences.compareTo(maxToleratedDifferences) > 0) {
 			if (DEBUG.messageEnabled()) {
-				DEBUG.message("Would be ignored if not more then " + maxToleratedDifferences + " differences");
+				DEBUG.message("Would be ignored if not more than " + maxToleratedDifferences + " differences");
 			}
             return new ComparisonResult(penaltyPoints);
 		}
@@ -119,18 +118,16 @@ public class MultiValueAttributeComparator {
 		if (percentageOfDifferences.compareTo(maxToleratedPercentage) > 0) {
 			if (DEBUG.messageEnabled()) {
 				DEBUG.message(percentageOfDifferences + " percents are different");
-				DEBUG.message("Would be ignored if not more then " + maxToleratedPercentage + " percent");
+				DEBUG.message("Would be ignored if not more than " + maxToleratedPercentage + " percent");
 			}
 			return new ComparisonResult(penaltyPoints);
 		}
 
         if (DEBUG.messageEnabled()) {
-            DEBUG.message("Ignored because number of differences(" + numberOfDifferences + ") not more then "
+            DEBUG.message("Ignored because number of differences(" + numberOfDifferences + ") not more than "
                     + maxToleratedDifferences);
-        }
-        if (DEBUG.messageEnabled()) {
             DEBUG.message(percentageOfDifferences + " percents are different");
-            DEBUG.message("Ignored because not more then " + maxToleratedPercentage + " percent");
+            DEBUG.message("Ignored because not more than " + maxToleratedPercentage + " percent");
         }
         return new ComparisonResult(true);
 	}
@@ -170,7 +167,7 @@ public class MultiValueAttributeComparator {
 	private BigDecimal getNumberOfSameElements(List<String> currentAttributes, List<String> storedAttributes) {
 		List<String> tmpCurrentAttributes = new ArrayList<String>(currentAttributes);
 		List<String> tmpStoredAttributes = new ArrayList<String>(storedAttributes);
-		
+
 		tmpCurrentAttributes.retainAll(tmpStoredAttributes);
 		return BigDecimal.valueOf(tmpCurrentAttributes.size());
 	}
