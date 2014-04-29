4b5074e38badfc23d9d12d2d47c0bb39a926197f
Mark de Reeper <mark.dereeper@forgerock.com>
2014-04-29 04:30:00 +0000

    Minor correction to test case missed from review feedback in fix for OPENAM-3920.

--- openam-federation/openam-federation-library/src/test/java/com/sun/identity/saml2/meta/SAML2MetaSecurityUtilsTest.java
+++ openam-federation/openam-federation-library/src/test/java/com/sun/identity/saml2/meta/SAML2MetaSecurityUtilsTest.java
@@ -18,7 +18,6 @@ package com.sun.identity.saml2.meta;
 
 import com.sun.identity.saml2.common.SAML2Exception;
 import com.sun.identity.shared.xml.XMLUtils;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.w3c.dom.Document;
 
@@ -28,15 +27,11 @@ public class SAML2MetaSecurityUtilsTest {
     private static final String SIGNED_XML_DOCUMENT = "signeddocument.xml";
 
     @Test
-    public void testVerifySignature() {
+    public void testVerifySignature() throws SAML2Exception {
 
         Document doc = XMLUtils.toDOMDocument(ClassLoader.getSystemResourceAsStream(SIGNED_XML_DOCUMENT),
                 SAML2MetaUtils.debug);
-        try {
-            // The keystore properties required to bootstrap the underlying key provider class are setup in the POM
-            SAML2MetaSecurityUtils.verifySignature(doc);
-        } catch (SAML2Exception e) {
-            Assert.fail(e.getMessage(), e);
-        }
+        // The keystore properties required to bootstrap the underlying key provider class are setup in the POM
+        SAML2MetaSecurityUtils.verifySignature(doc);
     }
 }
