fd910edac91ab0aed290634174efb2b60582c644
Neil Madden <neil.madden@forgerock.com>
2014-06-27 16:04:56 +0000

    AME-3787: CR-3882: Add file upload capability to ssoadm update-auth-instance

--- openam-cli/openam-cli-impl/src/main/java/com/sun/identity/cli/authentication/UpdateAuthInstance.java
+++ openam-cli/openam-cli-impl/src/main/java/com/sun/identity/cli/authentication/UpdateAuthInstance.java
@@ -26,6 +26,10 @@
  *
  */
 
+/*
+ * Portions Copyrighted 2014 Forgerock AS.
+ */
+
 package com.sun.identity.cli.authentication;
 
 import com.iplanet.sso.SSOException;
@@ -36,16 +40,22 @@ import com.sun.identity.authentication.config.AMConfigurationException;
 import com.sun.identity.cli.AttributeValues;
 import com.sun.identity.cli.AuthenticatedCommand;
 import com.sun.identity.cli.CLIException;
+import com.sun.identity.cli.CLIUtil;
 import com.sun.identity.cli.ExitCodes;
 import com.sun.identity.cli.IArgument;
 import com.sun.identity.cli.LogWriter;
 import com.sun.identity.cli.RequestContext;
 import com.sun.identity.log.Level;
 import com.sun.identity.sm.SMSException;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class UpdateAuthInstance extends AuthenticatedCommand {
+    private static final String FILE_REFERENCE_SUFFIX = "-file";
     /**
      * Handles request.
      *
@@ -69,9 +79,12 @@ public class UpdateAuthInstance extends AuthenticatedCommand {
                 getResourceString("missing-attributevalues"),
                 ExitCodes.INCORRECT_OPTION, rc.getSubCommand().getName());
         }
-        
-        Map attributeValues = AttributeValues.parse(
-            getCommandManager(), datafile, attrValues);
+
+        @SuppressWarnings("unchecked")
+        Map<String, Set<String>> attributeValues =
+                AttributeValues.parse(getCommandManager(), datafile, attrValues);
+
+        attributeValues = processFileAttributes(attributeValues);
         
         String[] params = {realm, instanceName};
         writeLog(LogWriter.LOG_ACCESS, Level.INFO,
@@ -112,5 +125,50 @@ public class UpdateAuthInstance extends AuthenticatedCommand {
                 "FAILED_UPDATE_AUTH_INSTANCE", params);
             throw new CLIException(e, ExitCodes.REQUEST_CANNOT_BE_PROCESSED);
         }
+    }
+
+    /**
+     * Post-process any attributes specified for the module instance (either via data file or on the command line) to
+     * resolve any file references. Any attribute can be specified using a -file suffix on the attribute name. This
+     * will cause the value to be treated as a file name, and the associated file to be read in (in the platform
+     * default encoding) and used as the attribute value. The attribute will be renamed to remove the -file suffix
+     * during this process.
+     *
+     * @param attrs the raw attributes read from the command line and/or data file.
+     * @return the processed attributes with all file references resolved.
+     * @throws CLIException if a referenced file cannot be read or if an attribute is specified both normally and using
+     * a -file reference.
+     */
+    private Map<String, Set<String>> processFileAttributes(Map<String, Set<String>> attrs) throws CLIException {
+        Map<String, Set<String>> result = attrs;
+        if (attrs != null) {
+            result = new LinkedHashMap<String, Set<String>>(attrs.size());
+
+            for (Map.Entry<String, Set<String>> attr : attrs.entrySet()) {
+                String key = attr.getKey();
+                Set<String> values = attr.getValue();
+
+                if (key != null && key.endsWith(FILE_REFERENCE_SUFFIX)) {
+                    key = key.substring(0, key.length() - FILE_REFERENCE_SUFFIX.length());
+
+                    if (attrs.containsKey(key)) {
+                        throw new CLIException("Cannot specify both normal and " + FILE_REFERENCE_SUFFIX
+                                + " attribute: " + key, ExitCodes.DUPLICATED_OPTION);
+                    }
+
+                    if (values != null) {
+                        Set<String> newValues = new LinkedHashSet<String>(values.size());
+                        for (String value : values) {
+                            newValues.add(CLIUtil.getFileContent(getCommandManager(), value));
+                        }
+                        values = newValues;
+                    }
+                }
+
+                result.put(key, values);
+            }
+
+        }
+        return result;
     }
 }
