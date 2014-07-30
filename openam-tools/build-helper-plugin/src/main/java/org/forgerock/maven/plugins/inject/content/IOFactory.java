6c1ac493fce86c6888cccbbf2aebc7069b61ddc4
Neil Madden <neil.madden@forgerock.com>
2014-07-30 10:10:23 +0000

    OPENAM-4249: CR-4120: Fix build failure on Windows (new Maven plugin)

--- openam-tools/build-helper-plugin/src/main/java/org/forgerock/maven/plugins/inject/content/IOFactory.java
+++ openam-tools/build-helper-plugin/src/main/java/org/forgerock/maven/plugins/inject/content/IOFactory.java
@@ -113,6 +113,12 @@ class IOFactory {
      * temporary file could not be deleted.
      */
     void moveTo(File fromFile, File toFile) throws IOException {
+        if (toFile.exists()) {
+            if (!toFile.delete()) {
+                throw new IOException("Failed to move file from " + fromFile + " to " + toFile.getAbsolutePath() +
+                        ": destination file exists and cannot be removed.");
+            }
+        }
         if (!fromFile.renameTo(toFile)) {
             throw new IOException("Failed to move file from, " + fromFile + ", to " + toFile.getAbsolutePath());
         }
