6bda13974cee16fc3f95a929bc88e3e8f92cd542
Mark de Reeper <mark.dereeper@forgerock.com>
2014-09-30 21:57:58 +0000

    Backport of OPENAM-4568

--- openam-tools/openam-installtools/src/test/java/com/sun/identity/install/tools/util/OSCheckerTest.java
+++ openam-tools/openam-installtools/src/test/java/com/sun/identity/install/tools/util/OSCheckerTest.java
@@ -16,55 +16,58 @@
 
 package com.sun.identity.install.tools.util;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.testng.Assert.*;
+import static com.sun.identity.install.tools.util.OSChecker.*;
 
 public class OSCheckerTest {
 
+    private static final String OS_NAME = System.getProperty("os.name");
+    private static final List<String> ARCHITECTURES = Arrays.asList(
+            "i386", "i686", "x86", "x86_64", "PowerPC", "ppc", "ppc64", "sparc");
+
     @Test
     public void testIsUnixOrWindows() {
-
-        Assert.assertTrue(OSChecker.isUnix() || OSChecker.isWindows());
+        assertTrue(isUnix() || isWindows());
+        assertFalse(isUnix() && isWindows());
     }
 
     @Test
     public void testLinuxVersionStringParsing() {
 
         String linuxVersionString = "2.6.32-279.14.1.el6.x86_64";
-        OSChecker.parseVersion(linuxVersionString);
-        Assert.assertEquals(OSChecker.getOsMajorVersion(), 2);
-        Assert.assertEquals(OSChecker.getOsMinorVersion(), 6);
-        // These are hard-coded in the POM
-        Assert.assertTrue(OSChecker.atleast("Linux", 2, 6));
-        Assert.assertTrue(OSChecker.matchArch("x86"));
+        parseVersion(linuxVersionString);
+        assertEquals(getOsMajorVersion(), 2);
+        assertEquals(getOsMinorVersion(), 6);
     }
 
     @Test
     public void testOSXVersionStringParsing() {
-
         String osXVersionString = "10.9.5";
-        OSChecker.parseVersion(osXVersionString);
-        Assert.assertEquals(OSChecker.getOsMajorVersion(), 10);
-        Assert.assertEquals(OSChecker.getOsMinorVersion(), 9);
-
+        parseVersion(osXVersionString);
+        assertEquals(getOsMajorVersion(), 10);
+        assertEquals(getOsMinorVersion(), 9);
     }
 
     @Test
     public void testWindowsVersionStringParsing() {
-
         String windowsVersionString = "5.1";
-        OSChecker.parseVersion(windowsVersionString);
-        Assert.assertEquals(OSChecker.getOsMajorVersion(), 5);
-        Assert.assertEquals(OSChecker.getOsMinorVersion(), 1);
+        parseVersion(windowsVersionString);
+        assertEquals(getOsMajorVersion(), 5);
+        assertEquals(getOsMinorVersion(), 1);
     }
 
     @Test
     public void testNoMinorVersionStringParsing() {
 
         String noMinorString = "100";
-        OSChecker.parseVersion(noMinorString);
-        Assert.assertEquals(OSChecker.getOsMajorVersion(), 100);
-        Assert.assertEquals(OSChecker.getOsMinorVersion(), 0);
+        parseVersion(noMinorString);
+        assertEquals(getOsMajorVersion(), 100);
+        assertEquals(getOsMinorVersion(), 0);
     }
 
     @Test
@@ -73,8 +76,32 @@ public class OSCheckerTest {
         // The exception output will end up in the TestCase debug log under the
         // target/test-cases directory as set in the POM.
         String exceptionString = "abc.10";
-        OSChecker.parseVersion(exceptionString);
-        Assert.assertEquals(OSChecker.getOsMajorVersion(), 0);
-        Assert.assertEquals(OSChecker.getOsMinorVersion(), 0);
+        parseVersion(exceptionString);
+        assertEquals(getOsMajorVersion(), 0);
+        assertEquals(getOsMinorVersion(), 0);
+    }
+
+    @Test
+    public void testAtLeast() {
+        parseVersion("1.2.3");
+        assertTrue(atleast(OS_NAME, 1, 0));
+        assertTrue(atleast(OS_NAME, 1, 2));
+        assertFalse(atleast(OS_NAME, 1, 3));
+        assertFalse(atleast(OS_NAME, 2, 1));
+        assertFalse(atleast("iOS", 1, 0));
+    }
+    
+    @Test
+    public void testArchitecture() {
+        int foundArch = 0;
+
+        // non-exhaustive list, covers conceivable architectures that might be running unit tests
+        for (String arch : ARCHITECTURES) {
+            if (matchArch(arch)) {
+                foundArch++;
+            }
+        }
+
+        assertEquals(foundArch, 1, "Expected " + System.getProperty("os.arch") + " to be one of " + ARCHITECTURES);
     }
 }
