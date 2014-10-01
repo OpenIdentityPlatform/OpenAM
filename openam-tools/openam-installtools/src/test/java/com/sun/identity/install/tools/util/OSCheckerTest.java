0a3e979529374fe2ce99e6b1658b77271eab90b2
James Phillpotts <james.phillpotts@forgerock.com>
2014-10-01 07:37:09 +0000

    OPENAM-4568 Support amd64 os.arch (reviewed in IM)

--- openam-tools/openam-installtools/src/test/java/com/sun/identity/install/tools/util/OSCheckerTest.java
+++ openam-tools/openam-installtools/src/test/java/com/sun/identity/install/tools/util/OSCheckerTest.java
@@ -28,7 +28,7 @@ public class OSCheckerTest {
 
     private static final String OS_NAME = System.getProperty("os.name");
     private static final List<String> ARCHITECTURES = Arrays.asList(
-            "i386", "i686", "x86", "x86_64", "PowerPC", "ppc", "ppc64", "sparc");
+            "i386", "i686", "x86", "x86_64", "amd64", "PowerPC", "ppc", "ppc64", "sparc");
 
     @Test
     public void testIsUnixOrWindows() {
