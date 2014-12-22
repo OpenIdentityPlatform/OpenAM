db368ea70e78ee7f7887ed7e39e30f761bc401c4
Mark de Reeper <mark.dereeper@forgerock.com>
2014-12-22 03:08:15 +0000

    Fix for OPENAM-2238, review CR-5565 - Support extensibility of auth context classes as described in the SAMLv2 spec

--- openam-console/src/main/java/com/sun/identity/console/federation/SAMLv2Base.java
+++ openam-console/src/main/java/com/sun/identity/console/federation/SAMLv2Base.java
@@ -24,108 +24,79 @@
  *
  * $Id: SAMLv2Base.java,v 1.8 2008/06/25 05:49:37 qcheng Exp $
  *
+ * Portions Copyrighted 2014 ForgeRock AS.
  */
 
 package com.sun.identity.console.federation;
 
 import com.iplanet.jato.model.ModelControlException;
 import com.iplanet.jato.view.event.DisplayEvent;
+import com.iplanet.jato.view.html.OptionList;
 import com.sun.identity.console.base.model.AMModel;
 import com.sun.identity.console.federation.model.EntityModel;
 import com.sun.identity.console.federation.model.SAMLv2ModelImpl;
+import com.sun.web.ui.model.CCActionTableModel;
+import com.sun.web.ui.view.html.CCDropDownMenu;
+
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 
 public abstract class SAMLv2Base extends EntityPropertiesBase {
     protected static final String PROPERTIES = "propertyAttributes";
-    
-    protected static List AUTH_CONTEXT_REF_NAMES = new ArrayList();    
-    
-    public static final int AUTH_CONTEXT_REF_COUNT= 25;
 
-    public static final String InternetProtocol =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:InternetProtocol";
-    public static final String InternetProtocolPassword =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:InternetProtocolPassword";
-    public static final String Kerberos =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:Kerberos";
-    public static final String MobileOneFactorUnregistered =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:MobileOneFactorUnregistered";
-    public static final String MobileTwoFactorUnregistered =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:MobileTwoFactorUnregistered";
-    public static final String MobileOneFactorContract =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:MobileOneFactorContract";
-    public static final String MobileTwoFactorContract =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:MobileTwoFactorContract";
-    public static final String Password =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:Password";
-    public static final String PasswordProtectedTransport =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport";
-    public static final String PreviousSession =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:PreviousSession";
-    public static final String X509 =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:X509";
-    public static final String PGP =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:PGP";
-    public static final String SPKI =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:SPKI";
-    public static final String XMLDSig =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:XMLDSig";
-    public static final String Smartcard =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:Smartcard";
-    public static final String SmartcardPKI =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:SmartcardPKI";
-    public static final String SoftwarePKI =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:SoftwarePKI";
-    public static final String Telephony =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:Telephony";
-    public static final String NomadTelephony =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:NomadTelephony";
-    public static final String PersonalTelephony =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:PersonalTelephony";
-    public static final String AuthenticatedTelephony =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:AuthenticatedTelephony";
-    public static final String SecureRemotePassword =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:SecureRemotePassword";
-    public static final String TLSClient =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:TLSClient";
-    public static final String TimeSyncToken =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:TimeSyncToken";
-    public static final String unspecified =
-        "urn:oasis:names:tc:SAML:2.0:ac:classes:unspecified";
-    
+    // Used to store the default list of supported contexts
+    private static final List<String> DEFAULT_AUTH_CONTEXT_REF_NAMES;
+
+    public static final String CHILD_AUTH_CONTEXT_TILED_VIEW = "tableTiledView";
+    public static final String TBL_AUTHENTICATION_CONTEXTS = "tblAuthenticationContext";
+    public static final String TBL_COL_SUPPORTED = "tblColSupported";
+    public static final String TBL_DATA_SUPPORTED = "tblDataSupported";
+    public static final String TBL_COL_CONTEXT_REFERENCE = "tblColContextReference";
+    public static final String TBL_DATA_CONTEXT_REFERENCE = "tblDataContextReference";
+    public static final String TBL_DATA_LABEL = "tblDataLabel";
+    public static final String TBL_COL_KEY = "tblColKey";
+    public static final String TBL_DATA_KEY = "tblDataKey";
+    public static final String TBL_COL_VALUE = "tblColValue";
+    public static final String TBL_DATA_VALUE = "tblDataValue";
+    public static final String TBL_COL_LEVEL = "tblColLevel";
+    public static final String TBL_DATA_LEVEL = "tblDataLevel";
+
     static {
-        AUTH_CONTEXT_REF_NAMES.add(InternetProtocol);
-        AUTH_CONTEXT_REF_NAMES.add(InternetProtocolPassword);
-        AUTH_CONTEXT_REF_NAMES.add(Kerberos);
-        AUTH_CONTEXT_REF_NAMES.add(MobileOneFactorUnregistered);
-        AUTH_CONTEXT_REF_NAMES.add(MobileTwoFactorUnregistered);
-        AUTH_CONTEXT_REF_NAMES.add(MobileOneFactorContract);
-        AUTH_CONTEXT_REF_NAMES.add(MobileTwoFactorContract);
-        AUTH_CONTEXT_REF_NAMES.add(Password);
-        AUTH_CONTEXT_REF_NAMES.add(PasswordProtectedTransport);
-        AUTH_CONTEXT_REF_NAMES.add(PreviousSession);
-        AUTH_CONTEXT_REF_NAMES.add(X509);
-        AUTH_CONTEXT_REF_NAMES.add(PGP);
-        AUTH_CONTEXT_REF_NAMES.add(SPKI);
-        AUTH_CONTEXT_REF_NAMES.add(XMLDSig);
-        AUTH_CONTEXT_REF_NAMES.add(Smartcard);
-        AUTH_CONTEXT_REF_NAMES.add(SmartcardPKI);
-        AUTH_CONTEXT_REF_NAMES.add(SoftwarePKI);
-        AUTH_CONTEXT_REF_NAMES.add(Telephony);
-        AUTH_CONTEXT_REF_NAMES.add(NomadTelephony);
-        AUTH_CONTEXT_REF_NAMES.add(PersonalTelephony);
-        AUTH_CONTEXT_REF_NAMES.add(AuthenticatedTelephony);
-        AUTH_CONTEXT_REF_NAMES.add(SecureRemotePassword);
-        AUTH_CONTEXT_REF_NAMES.add(TLSClient);
-        AUTH_CONTEXT_REF_NAMES.add(TimeSyncToken);
-        AUTH_CONTEXT_REF_NAMES.add(unspecified);
+        List<String> defaultContextNames = new ArrayList<String>();
+        // The default set of context's shown in the Hosted IDP/SP pages
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:InternetProtocol");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:InternetProtocolPassword");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:Kerberos");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:MobileOneFactorUnregistered");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:MobileTwoFactorUnregistered");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:MobileOneFactorContract");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:MobileTwoFactorContract");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:Password");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:PreviousSession");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:X509");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:PGP");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:SPKI");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:XMLDSig");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:Smartcard");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:SmartcardPKI");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:SoftwarePKI");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:Telephony");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:NomadTelephony");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:PersonalTelephony");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:AuthenticatedTelephony");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:SecureRemotePassword");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:TLSClient");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:TimeSyncToken");
+        defaultContextNames.add("urn:oasis:names:tc:SAML:2.0:ac:classes:unspecified");
+
+        DEFAULT_AUTH_CONTEXT_REF_NAMES = Collections.unmodifiableList(defaultContextNames);
     }
     
     public SAMLv2Base(String name) {
@@ -168,5 +139,88 @@ public abstract class SAMLv2Base extends EntityPropertiesBase {
      */
     protected Set returnEmptySetIfValueIsNull(Set set) {
         return (set != null) ? set : Collections.EMPTY_SET;
+    }
+
+    /**
+     * For the given set of authContexts read from the extended metadata, populate the table and dropdown menu
+     * @param authContexts The set of SAMLv2AuthContexts read from the extended metadata
+     * @param tblAuthContextsModel The table model to populate
+     * @param dropdownContextRef The name of the context dropdown menu component to populate
+     */
+    protected void populateAuthenticationContext(SAMLv2AuthContexts authContexts,
+                                                 CCActionTableModel tblAuthContextsModel, String dropdownContextRef) {
+
+        // Create lists from defaults that can be updated as there maybe custom entries from the extended metadata.
+        Set<String> contextNames = new LinkedHashSet<String>(DEFAULT_AUTH_CONTEXT_REF_NAMES);
+        OptionList options = new OptionList();
+        // Used to indicate no default context
+        options.add(getLabel("none"), "none");
+        for (String name : contextNames) {
+            options.add(getLabel(name), name);
+        }
+
+        // Need to compare the list from the metadata to the default list and any that are missing should be added
+        // to the set being shown in the console as they will be custom entries
+        Map<String, SAMLv2AuthContexts.SAMLv2AuthContext> contexts = authContexts.getCollections();
+        for (SAMLv2AuthContexts.SAMLv2AuthContext value : contexts.values()) {
+            if (contextNames.add(value.name)) {
+                options.add(getLabel(value.name), value.name);
+            }
+        }
+
+        CCDropDownMenu ac = (CCDropDownMenu) getChild(dropdownContextRef);
+        ac.setOptions(options);
+
+        tblAuthContextsModel.clear();
+        int i = 0;
+        for (String name : contextNames) {
+            populateAuthenticationContext(name, authContexts, i++, tblAuthContextsModel, dropdownContextRef);
+        }
+    }
+
+    private void populateAuthenticationContext(String name, SAMLv2AuthContexts authContexts, int index,
+        CCActionTableModel tblAuthContextsModel, String dropdownContextRef) {
+
+        if (index != 0) {
+            tblAuthContextsModel.appendRow();
+        }
+
+        tblAuthContextsModel.setValue(TBL_DATA_CONTEXT_REFERENCE, name);
+        tblAuthContextsModel.setValue(TBL_DATA_LABEL, getLabel(name));
+
+        SAMLv2AuthContexts.SAMLv2AuthContext authContextObj = null;
+        if (authContexts != null) {
+            authContextObj = authContexts.get(name);
+        }
+
+        if (authContextObj == null) {
+            tblAuthContextsModel.setValue(TBL_DATA_SUPPORTED, "");
+            tblAuthContextsModel.setValue(TBL_DATA_LEVEL, "0");
+        } else {
+            tblAuthContextsModel.setValue(TBL_DATA_SUPPORTED, authContextObj.supported);
+            tblAuthContextsModel.setValue(TBL_DATA_LEVEL, authContextObj.level);
+            if (authContextObj.isDefault) {
+                setDisplayFieldValue(dropdownContextRef, authContextObj.name);
+            }
+        }
+    }
+
+    private String getLabel(String name) {
+
+        String key = getAuthContextI18nKey(name);
+        String label = getModelInternal().getLocalizedString(key);
+        // If the label ends up being the same as the key then there was no corresponding localized string so very
+        // likely a custom context, just use the context name instead.
+        if (label.equals(key)) {
+            label = name;
+        }
+
+        return label;
+    }
+
+    private String getAuthContextI18nKey(String name) {
+        int idx = name.lastIndexOf(":");
+        String key = (idx != -1) ? name.substring(idx + 1) : name;
+        return "samlv2.authenticationContext." + key + ".label";
     }
 }
