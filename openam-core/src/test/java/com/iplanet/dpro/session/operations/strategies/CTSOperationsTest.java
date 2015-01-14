cf70c5c2491ffe5d23f570d4b92a1e8baa73fd42
Craig McDonnell <craig.mcdonnell@forgerock.com>
2015-01-14 09:45:32 +0000

    OPENAM-4717 CR-5785 Update licence

--- openam-core/src/test/java/com/iplanet/dpro/session/operations/strategies/CTSOperationsTest.java
+++ openam-core/src/test/java/com/iplanet/dpro/session/operations/strategies/CTSOperationsTest.java
@@ -11,7 +11,7 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions copyright [year] [name of copyright owner]".
  *
- * Copyright 2013-2014 ForgeRock AS.
+ * Copyright 2013-2015 ForgeRock AS.
  */
 package com.iplanet.dpro.session.operations.strategies;
 
