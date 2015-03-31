55e4c7f8378faa0117d92fe5f2cae87fa9c5bebf
Quentin CASTEL <quentin.castel@forgerock.com>
2015-03-31 12:29:37 +0000

    OPENAM-2270 RESTful Auth API does not correctly preserve input values in membership module

--- openam-forgerock-rest/src/main/java/org/forgerock/openam/forgerockrest/authn/RestAuthCallbackHandlerManager.java
+++ openam-forgerock-rest/src/main/java/org/forgerock/openam/forgerockrest/authn/RestAuthCallbackHandlerManager.java
@@ -11,7 +11,7 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions copyright [year] [name of copyright owner]".
  *
- * Copyright 2013 ForgeRock AS.
+ * Copyright 2013-2015 ForgeRock AS.
  */
 
 package org.forgerock.openam.forgerockrest.authn;
@@ -19,6 +19,7 @@ package org.forgerock.openam.forgerockrest.authn;
 import com.google.inject.Singleton;
 import com.sun.identity.shared.debug.Debug;
 import org.forgerock.json.fluent.JsonValue;
+import org.forgerock.json.resource.ResourceException;
 import org.forgerock.openam.forgerockrest.authn.callbackhandlers.RestAuthCallbackHandler;
 import org.forgerock.openam.forgerockrest.authn.callbackhandlers.RestAuthCallbackHandlerResponseException;
 import org.forgerock.openam.forgerockrest.authn.exceptions.RestAuthException;
@@ -132,31 +133,31 @@ public class RestAuthCallbackHandlerManager {
      * @param jsonCallbacks The JSON representation of the Callbacks.
      * @return The same Callbacks as in the parameters with the required values set.
      */
-    public Callback[] handleJsonCallbacks(Callback[] originalCallbacks, JsonValue jsonCallbacks) {
+    public Callback[] handleJsonCallbacks(final Callback[] originalCallbacks, final JsonValue jsonCallbacks)
+            throws RestAuthException {
 
-        for (int j = 0; j < originalCallbacks.length; j++) {
+        if (originalCallbacks.length != jsonCallbacks.size()) {
+            logger.error("Incorrect number of callbacks found in JSON response");
+            throw new RestAuthException(ResourceException.BAD_REQUEST,
+                    "Incorrect number of callbacks found in JSON response");
+        }
 
-            RestAuthCallbackHandler restAuthCallbackHandler =
-                    restAuthCallbackHandlerFactory.getRestAuthCallbackHandler(originalCallbacks[j].getClass());
+        for (int i = 0; i < originalCallbacks.length; i++) {
 
-            boolean foundParser = false;
+            final Callback originalCallback = originalCallbacks[i];
 
-            for (int i = 0; i < jsonCallbacks.size(); i++) {
+            final RestAuthCallbackHandler restAuthCallbackHandler =
+                    restAuthCallbackHandlerFactory.getRestAuthCallbackHandler(originalCallback.getClass());
 
-                JsonValue jsonCallback = jsonCallbacks.get(i);
+            final JsonValue jsonCallback = jsonCallbacks.get(i);
 
-                if (restAuthCallbackHandler.getCallbackClassName().equals(jsonCallback.get("type").asString())) {
-                    foundParser = true;
-                    restAuthCallbackHandler.convertFromJson(originalCallbacks[j], jsonCallback);
-                    break;
-                }
-            }
-
-            if (!foundParser) {
+            if (!restAuthCallbackHandler.getCallbackClassName().equals(jsonCallback.get("type").asString())) {
                 logger.error("Required callback not found in JSON response");
                 throw new RestAuthException(Response.Status.BAD_REQUEST,
                         "Required callback not found in JSON response");
             }
+
+            restAuthCallbackHandler.convertFromJson(originalCallback, jsonCallback);
         }
 
         return originalCallbacks;
