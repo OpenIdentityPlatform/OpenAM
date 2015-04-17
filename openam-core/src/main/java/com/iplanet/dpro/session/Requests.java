3c3c451c82a2315356f9275ebc18f65c8e75ad70
Sachiko Wallace <sachiko.wallace@forgerock.com>
2015-04-17 02:49:18 +0000

    OPENAM-5835 CR-6660 make sure admin token gets refreshed in case of exception session response

--- openam-core/src/main/java/com/iplanet/dpro/session/Requests.java
+++ openam-core/src/main/java/com/iplanet/dpro/session/Requests.java
@@ -116,6 +116,7 @@ public class Requests {
             sres = pllSender.sendPLLRequest(svcurl, sreq);
             while (sres.getException() != null) {
                 session.processSessionResponseException(sres, appSSOToken);
+                context = session.getContext();
                 if (context != null) {
                     sreq.setRequester(RestrictedTokenContext.marshal(context));
                 }
