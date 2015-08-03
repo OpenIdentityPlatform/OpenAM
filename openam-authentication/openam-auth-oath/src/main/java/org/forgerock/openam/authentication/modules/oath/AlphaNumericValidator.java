9d2daabb69fe4a4a8b257c325adeedaf2a80f0e7
David Luna <david.luna@forgerock.com>
2015-08-03 16:47:17 +0000

    OPENAM-6403, CR-7799 - missing validators

---
+++ openam-authentication/openam-auth-oath/src/main/java/org/forgerock/openam/authentication/modules/oath/AlphaNumericValidator.java
@@ -0,0 +1,48 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2015 ForgeRock AS.
+ */
+
+package org.forgerock.openam.authentication.modules.oath;
+
+import com.sun.identity.sm.ServiceAttributeValidator;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+/**
+ * Service Manager validator for alpha numeric characters.
+ */
+public class AlphaNumericValidator implements ServiceAttributeValidator {
+
+    private static final Pattern NOT_ALPHA_NUM = Pattern.compile("[^a-zA-Z0-9]");
+
+    /**
+     * Validates each of the provided members of the Set to confirm that it conforms
+     * to the expected validation - only a-z/A-Z, 0-9.
+     *
+     * @param values the <code>Set</code> of attribute values to validate
+     * @return true if everything is valid, false otherwise.
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean validate(Set values) {
+        for (String toTest : (Set<String>) values) {
+            if (NOT_ALPHA_NUM.matcher(toTest).find()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ openam-authentication/openam-auth-oath/src/main/java/org/forgerock/openam/authentication/modules/oath/CodeLengthValidator.java
@@ -0,0 +1,50 @@
+/*
+* The contents of this file are subject to the terms of the Common Development and
+* Distribution License (the License). You may not use this file except in compliance with the
+* License.
+*
+* You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+* specific language governing permission and limitations under the License.
+*
+* When distributing Covered Software, include this CDDL Header Notice in each file and include
+* the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+* Header, with the fields enclosed by brackets [] replaced by your own identifying
+* information: "Portions copyright [year] [name of copyright owner]".
+*
+* Copyright 2015 ForgeRock AS.
+*/
+package org.forgerock.openam.authentication.modules.oath;
+
+import com.sun.identity.sm.ServiceAttributeValidator;
+import java.util.Set;
+
+/**
+ * Validates that the entered code length is at least 6.
+ */
+public class CodeLengthValidator implements ServiceAttributeValidator {
+
+    public static final int MIN_CODE_LENGTH = 6;
+
+    /**
+     * Validates each of the provided members of the Set to confirm that they are
+     * greater than the
+     *
+     * @param values the <code>Set</code> of attribute values to validate
+     * @return true if everything is valid, false otherwise.
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean validate(Set values) {
+        try {
+            for (String toTest : (Set<String>) values) {
+                if (Integer.valueOf(toTest) < MIN_CODE_LENGTH) {
+                    return false;
+                }
+            }
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+
+        return true;
+    }
+}
