ff61673cd5d9fe33dfc9078d5542d8c5d409fdd6
Kamal Sivanandam <kamal.sivanandam@forgerock.com>
2015-09-24 12:25:07 +0000

    OPENAM-6842,CR-8280  If applied, this commit will enable to get the Post Authentication Process Instance from the SSOToken, if InternalSession is null.

--- openam-core/src/main/java/com/sun/identity/authentication/service/AuthUtils.java
+++ openam-core/src/main/java/com/sun/identity/authentication/service/AuthUtils.java
@@ -2024,26 +2024,29 @@ public class AuthUtils extends AuthClientUtils {
             
             if (intSession != null) {
                 plis = intSession.getProperty(ISAuthConstants.POST_AUTH_PROCESS_INSTANCE);
+            } else {
+                plis = token.getProperty(ISAuthConstants.POST_AUTH_PROCESS_INSTANCE);
+                if (utilDebug.messageEnabled()) {
+                    utilDebug.message("InternalSession is null, obtaining PAP instance from ssotoken");
+                }
             }
             if (plis != null && plis.length() > 0) {
                 StringTokenizer st = new StringTokenizer(plis, "|");
                 
-                if (token != null) {
-                    while (st.hasMoreTokens()) {
-                        String pli = (String)st.nextToken();
+
+                while (st.hasMoreTokens()) {
+                    String pli = (String)st.nextToken();
                         
-                        try {
-                            AMPostAuthProcessInterface postProcess = 
-                                    (AMPostAuthProcessInterface)
-                                    Thread.currentThread().
-                                    getContextClassLoader().
-                                    loadClass(pli).newInstance();
+                    try {
+                        AMPostAuthProcessInterface postProcess =
+                                    (AMPostAuthProcessInterface) Thread.currentThread().
+                                    getContextClassLoader().loadClass(pli).newInstance();
                             postProcess.onLogout(request, response, token);
-                        } catch (Exception ex) {
+                    } catch (Exception ex) {
                             utilDebug.error("AuthUtils.logout:" + pli, ex);
-                        }
                     }
                 }
+
             }
         }
         
@@ -2052,7 +2055,7 @@ public class AuthUtils extends AuthClientUtils {
         try {
             isTokenValid = SSOTokenManager.getInstance().isValidToken(token);
             
-            if ((token != null) && isTokenValid) {
+            if (isTokenValid) {
                 AuthD.getAuth().logLogout(token);
                 SSOTokenManager.getInstance().logout(token);
 
