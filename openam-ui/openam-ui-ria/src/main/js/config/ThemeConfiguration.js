30e50ddbaff38a048aeabb40eb7779757e1162e3
Julian Kigwana <julian.kigwana@forgerock.com>
2015-10-15 15:06:36 +0100

    AME-8698 Make UMA side navigation theme-able. Fixing css ordering. Fixed bug with UMA Resources title trying to redirect user to the console realms view! Pulled page-headers into there own less files

--- openam-ui/openam-ui-ria/src/main/js/config/ThemeConfiguration.js
+++ openam-ui/openam-ui-ria/src/main/js/config/ThemeConfiguration.js
@@ -19,7 +19,7 @@ define("config/ThemeConfiguration", {
         // There must be a theme named "default".
         "default": {
             // An ordered list of URLs to stylesheets that will be applied to every page.
-            stylesheets: ["css/bootstrap-3.3.5-custom.css", "css/theme.css", "css/structure.css"],
+            stylesheets: ["css/bootstrap-3.3.5-custom.css", "css/structure.css", "css/theme.css"],
             // A path that is prepended to every relative URL when fetching resources (including images, stylesheets and
             // HTML template files).
             path: "",
