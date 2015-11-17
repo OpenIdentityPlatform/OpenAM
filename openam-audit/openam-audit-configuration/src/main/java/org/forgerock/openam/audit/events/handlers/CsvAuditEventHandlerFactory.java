d04cb187fca1113d2306bbd6157bbc7cad75db7b
Brian Bailey <brian.bailey@forgerock.com>
2015-11-17 12:11:26 +0000

    AME-8098 Exposing rotation and retention config.

--- openam-audit/openam-audit-configuration/src/main/java/org/forgerock/openam/audit/events/handlers/CsvAuditEventHandlerFactory.java
+++ openam-audit/openam-audit-configuration/src/main/java/org/forgerock/openam/audit/events/handlers/CsvAuditEventHandlerFactory.java
@@ -20,6 +20,7 @@ import static com.sun.identity.shared.Constants.AM_SERVICES_DEPLOYMENT_DESCRIPTO
 import static com.sun.identity.shared.datastruct.CollectionHelper.*;
 
 import com.iplanet.am.util.SystemProperties;
+import com.sun.identity.shared.debug.Debug;
 import org.forgerock.audit.AuditException;
 import org.forgerock.audit.events.handlers.AuditEventHandler;
 import org.forgerock.audit.handlers.csv.CsvAuditEventHandler;
@@ -27,8 +28,11 @@ import org.forgerock.audit.handlers.csv.CsvAuditEventHandlerConfiguration;
 import org.forgerock.audit.providers.DefaultSecureStorageProvider;
 import org.forgerock.openam.audit.AuditEventHandlerFactory;
 import org.forgerock.openam.audit.configuration.AuditEventHandlerConfiguration;
+import org.forgerock.openam.utils.StringUtils;
 
 import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -39,6 +43,8 @@ import java.util.Set;
  */
 @Singleton
 public class CsvAuditEventHandlerFactory implements AuditEventHandlerFactory {
+
+    private final Debug debug = Debug.getInstance("amAudit");
 
     @Override
     public AuditEventHandler create(AuditEventHandlerConfiguration configuration) throws AuditException {
@@ -51,8 +57,43 @@ public class CsvAuditEventHandlerFactory implements AuditEventHandlerFactory {
         csvHandlerConfiguration.setTopics(attributes.get("topics"));
         csvHandlerConfiguration.setName(configuration.getHandlerName());
         csvHandlerConfiguration.setEnabled(getBooleanMapAttr(attributes, "enabled", true));
+        setFileRotationPolicies(csvHandlerConfiguration, attributes);
+        setFileRetentionPolicies(csvHandlerConfiguration, attributes);
 
         return new CsvAuditEventHandler(csvHandlerConfiguration, configuration.getEventTopicsMetaData(),
-                new DefaultSecureStorageProvider());
+            new DefaultSecureStorageProvider());
+    }
+
+    private void setFileRotationPolicies(CsvAuditEventHandlerConfiguration csvHandlerConfiguration,
+            Map<String, Set<String>> attributes) {
+        boolean enabled = getBooleanMapAttr(attributes, "rotationEnabled", false);
+        csvHandlerConfiguration.getFileRotation().setRotationEnabled(enabled);
+        String maxFileSizeAttribute = getMapAttr(attributes, "rotationMaxFileSize");
+        if (StringUtils.isNotEmpty(maxFileSizeAttribute)) {
+            long maxFileSize = Long.parseLong(maxFileSizeAttribute);
+            csvHandlerConfiguration.getFileRotation().setMaxFileSize(maxFileSize);
+        }
+        String filePrefix = getMapAttr(attributes, "rotationFilePrefix");
+        csvHandlerConfiguration.getFileRotation().setRotationFilePrefix(filePrefix);
+        String fileSuffix = getMapAttr(attributes, "rotationFileSuffix");
+        csvHandlerConfiguration.getFileRotation().setRotationFileSuffix(fileSuffix);
+        String interval = getMapAttr(attributes, "rotationInterval");
+        csvHandlerConfiguration.getFileRotation().setRotationInterval(interval);
+        List<String> times = new ArrayList<>();
+        Set<String> rotationTimesAttribute = attributes.get("rotationTimes");
+        if (rotationTimesAttribute != null && !rotationTimesAttribute.isEmpty()) {
+            times.addAll(rotationTimesAttribute);
+            csvHandlerConfiguration.getFileRotation().setRotationTimes(times);
+        }
+    }
+
+    private void setFileRetentionPolicies(CsvAuditEventHandlerConfiguration csvHandlerConfiguration,
+            Map<String, Set<String>> attributes) {
+        int maxNumberOfHistoryFiles = getIntMapAttr(attributes, "retentionMaxNumberOfHistoryFiles", -1, debug);
+        csvHandlerConfiguration.getFileRetention().setMaxNumberOfHistoryFiles(maxNumberOfHistoryFiles);
+        long maxDiskSpaceToUse = getLongMapAttr(attributes, "retentionMaxDiskSpaceToUse", -1L, debug);
+        csvHandlerConfiguration.getFileRetention().setMaxDiskSpaceToUse(maxDiskSpaceToUse);
+        int minFreeSpaceRequired = getIntMapAttr(attributes, "retentionMinFreeSpaceRequired", -1, debug);
+        csvHandlerConfiguration.getFileRetention().setMinFreeSpaceRequired(minFreeSpaceRequired);
     }
 }
