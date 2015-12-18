8c6010e199d16d726992f136083886045f445334
Andrew Forrest <andrew.forrest@forgerock.com>
2015-12-18 12:29:52 +0000

    AME-9360 new collection to action to copy/move all policies

--- openam-entitlements/src/main/java/org/forgerock/openam/entitlement/rest/PolicyResourceWithCopyMoveSupport.java
+++ openam-entitlements/src/main/java/org/forgerock/openam/entitlement/rest/PolicyResourceWithCopyMoveSupport.java
@@ -16,6 +16,12 @@
 
 package org.forgerock.openam.entitlement.rest;
 
+import static org.forgerock.json.JsonValue.array;
+import static org.forgerock.json.JsonValue.field;
+import static org.forgerock.json.JsonValue.json;
+import static org.forgerock.json.JsonValue.object;
+
+import org.forgerock.json.JsonPointer;
 import org.forgerock.json.JsonValue;
 import org.forgerock.json.resource.ActionRequest;
 import org.forgerock.json.resource.ActionResponse;
@@ -23,9 +29,12 @@ import org.forgerock.json.resource.BadRequestException;
 import org.forgerock.json.resource.CollectionResourceProvider;
 import org.forgerock.json.resource.CreateRequest;
 import org.forgerock.json.resource.DeleteRequest;
+import org.forgerock.json.resource.QueryRequest;
+import org.forgerock.json.resource.QueryResourceHandler;
 import org.forgerock.json.resource.ReadRequest;
 import org.forgerock.json.resource.Requests;
 import org.forgerock.json.resource.ResourceException;
+import org.forgerock.json.resource.ResourceResponse;
 import org.forgerock.json.resource.Responses;
 import org.forgerock.json.resource.Router;
 import org.forgerock.openam.rest.RealmContext;
@@ -34,9 +43,15 @@ import org.forgerock.services.context.Context;
 import org.forgerock.util.Reject;
 import org.forgerock.util.promise.Promise;
 import org.forgerock.util.promise.Promises;
+import org.forgerock.util.query.QueryFilter;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Adds additional behaviour to the existing {@link PolicyResource} to support the move and copy of policies.
@@ -53,6 +68,125 @@ final class PolicyResourceWithCopyMoveSupport extends DecoratedCollectionResourc
         super(wrappedResource);
         Reject.ifNull(router);
         this.router = router;
+    }
+
+    @Override
+    public Promise<ActionResponse, ResourceException> actionCollection(Context context, ActionRequest actionRequest) {
+        String actionString = actionRequest.getAction();
+        PolicyAction action = PolicyAction.getAction(actionString);
+
+        try {
+            switch (action) {
+            case MOVE:
+            case COPY:
+                return Promises.newResultPromise(copyOrMovePoliciesByApplication(context, actionRequest, action));
+            default:
+                return super.actionCollection(context, actionRequest);
+            }
+        } catch (ResourceException rE) {
+            return rE.asPromise();
+        }
+
+    }
+
+    private ActionResponse copyOrMovePoliciesByApplication(Context context,
+            ActionRequest request, PolicyAction copyOrMoveAction) throws ResourceException {
+
+        JsonValue payload = request.getContent();
+        JsonValue from = payload.get("from");
+        JsonValue to = payload.get("to");
+
+        if (from.isNull()) {
+            throw new BadRequestException("from definition is missing");
+        }
+
+        if (!from.isDefined("application")) {
+            throw new BadRequestException("from application definition is missing");
+        }
+
+        String sourceApplication = from
+                .get("application")
+                .asString();
+
+        if (to.isNull()) {
+            throw new BadRequestException("to definition is missing");
+        }
+
+        String sourceRealm = RealmContext.getRealm(context);
+
+        String destinationRealm = to
+                .get("realm")
+                .defaultTo(sourceRealm)
+                .asString();
+
+        String destinationApplication = to
+                .get("application")
+                .defaultTo(sourceApplication)
+                .asString();
+
+        JsonValue resourceTypeMapping = payload
+                .get("resourceTypeMapping")
+                .defaultTo(Collections.emptyMap());
+
+        String namePostfix = to
+                .get("namePostfix")
+                .defaultTo("")
+                .asString();
+
+        QueryRequest queryRequest = Requests.newQueryRequest("policies");
+        queryRequest.setQueryFilter(QueryFilter.equalTo(new JsonPointer("applicationName"), sourceApplication));
+
+        final List<JsonValue> policies = new ArrayList<>();
+        router.handleQuery(context, queryRequest, new QueryResourceHandler() {
+
+            @Override
+            public boolean handleResource(ResourceResponse resourceResponse) {
+                policies.add(resourceResponse.getContent());
+                return true;
+            }
+
+        }).getOrThrowUninterruptibly();
+
+        JsonValue actionResponseContent = json(array());
+
+        for (JsonValue policy : policies) {
+            ActionResponse response = copyOrMoveGivenPolicy(context, policy, destinationRealm,
+                    destinationApplication, namePostfix, resourceTypeMapping, copyOrMoveAction);
+            actionResponseContent.add(response.getJsonContent().asMap());
+        }
+
+        return Responses.newActionResponse(actionResponseContent);
+    }
+
+    private ActionResponse copyOrMoveGivenPolicy(Context context, JsonValue policy, String destinationRealm,
+            String destinationApplication, String namePostfix, JsonValue resourceTypeMapping,
+            PolicyAction copyOrMoveAction) throws ResourceException {
+
+        String name = policy.get("name").asString();
+        String copiedName = name + namePostfix;
+
+        String sourceResourceType = policy.get("resourceTypeUuid").asString();
+        String destinationResourceType = resourceTypeMapping
+                .get(sourceResourceType)
+                .defaultTo(sourceResourceType)
+                .asString();
+
+        JsonValue newPayload = json(
+                object(
+                        field("to",
+                                object(
+                                        field("name", copiedName),
+                                        field("realm", destinationRealm),
+                                        field("application", destinationApplication),
+                                        field("resourceType", destinationResourceType)))));
+
+        String copyOrMoveActionName = copyOrMoveAction.name().toLowerCase();
+        ActionRequest newActionRequest = Requests.newActionRequest("policies", name, copyOrMoveActionName);
+        newActionRequest.setContent(newPayload);
+
+        return router
+                .handleAction(context, newActionRequest)
+                .getOrThrowUninterruptibly();
     }
 
     @Override
