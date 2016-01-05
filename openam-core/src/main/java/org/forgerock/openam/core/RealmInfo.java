25966617d1bd64d0d70cf51975076a404287ff83
Peter Major <peter.major@forgerock.com>
2016-01-05 15:52:35 +0000

    OPENAM-8004 Fix equals implementation for RealmInfo

--- openam-core/src/main/java/org/forgerock/openam/core/RealmInfo.java
+++ openam-core/src/main/java/org/forgerock/openam/core/RealmInfo.java
@@ -11,7 +11,7 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions copyright [year] [name of copyright owner]".
  *
- * Copyright 2015 ForgeRock AS.
+ * Copyright 2015-2016 ForgeRock AS.
  */
 
 package org.forgerock.openam.core;
@@ -105,7 +105,7 @@ public class RealmInfo {
         RealmInfo realmInfo = (RealmInfo) o;
 
         return Objects.equals(absoluteRealm, realmInfo.absoluteRealm)
-                && Objects.equals(realmInfo, realmInfo.realmSubPath)
+                && Objects.equals(realmSubPath, realmInfo.realmSubPath)
                 && Objects.equals(overrideRealm, realmInfo.overrideRealm);
     }
 
