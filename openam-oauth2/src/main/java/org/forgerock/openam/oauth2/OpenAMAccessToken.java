294b358ba9879832db18b440c5653a50eb7f4d58
Ram Anaswara <ram.anaswara@forgerock.com>
2016-02-09 11:13:06 +0000

    OPENAM-8142 Create access token with correct client name, when the client contains a space in their name

--- openam-oauth2/src/main/java/org/forgerock/openam/oauth2/OpenAMAccessToken.java
+++ openam-oauth2/src/main/java/org/forgerock/openam/oauth2/OpenAMAccessToken.java
@@ -11,7 +11,7 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions copyright [year] [name of copyright owner]".
  *
- * Copyright 2014-2015 ForgeRock AS.
+ * Copyright 2014-2016 ForgeRock AS.
  */
 
 package org.forgerock.openam.oauth2;
@@ -126,7 +126,7 @@ public class OpenAMAccessToken extends AccessToken {
      */
     @Override
     protected void setClientId(String clientId) {
-        put(CLIENT_ID, stringToSet(clientId));
+        put(CLIENT_ID, CollectionUtils.asSet(clientId));
     }
 
     /**
