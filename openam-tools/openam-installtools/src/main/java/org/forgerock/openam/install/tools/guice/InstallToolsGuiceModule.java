0307db0d7f3e9815ad97694079380fb05da7f4b7
Tony Bamford <tony.bamford@forgerock.com>
2016-03-23 10:01:06 +0000

    AME-9952: Update copyright dates

--- openam-tools/openam-installtools/src/main/java/org/forgerock/openam/install/tools/guice/InstallToolsGuiceModule.java
+++ openam-tools/openam-installtools/src/main/java/org/forgerock/openam/install/tools/guice/InstallToolsGuiceModule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 ForgeRock, AS.
+ * Copyright 2016 ForgeRock, AS.
  *
  * The contents of this file are subject to the terms of the Common Development and
  * Distribution License (the License). You may not use this file except in compliance with the
