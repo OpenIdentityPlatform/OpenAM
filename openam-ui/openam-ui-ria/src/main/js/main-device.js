9b360e9337074d01760986b660d4e8f2658cc1ab
Phil Ostler <phil.ostler@forgerock.com>
2016-08-04 14:39:14 +0100

    AME-11268 Add Redux path mapping to other main-*.js files

--- openam-ui/openam-ui-ria/src/main/js/main-device.js
+++ openam-ui/openam-ui-ria/src/main/js/main-device.js
@@ -24,11 +24,12 @@ require.config({
         }
     },
     paths: {
-        "lodash":       "libs/lodash-3.10.1-min",
-        "handlebars":   "libs/handlebars-4.0.5",
-        "i18next":      "libs/i18next-1.7.3-min",
-        "jquery":       "libs/jquery-2.1.1-min",
-        "text":         "libs/text-2.0.15"
+        "handlebars": "libs/handlebars-4.0.5",
+        "i18next": "libs/i18next-1.7.3-min",
+        "jquery": "libs/jquery-2.1.1-min",
+        "lodash": "libs/lodash-3.10.1-min",
+        "redux": "libs/redux-3.5.2-min",
+        "text": "libs/text-2.0.15"
     },
     shim: {
         "handlebars": {
