5515be284938e3e9a8c4cfea272bf5475a1e75c9
Quentin CASTEL <quentin.castel@forgerock.com>
2016-11-08 12:20:19 +0000

    OPENAM-9760 OpenID Connect validation configuration value" should not be used with client_secret

--- openam-authentication/openam-auth-oidc/src/main/java/org/forgerock/openam/authentication/modules/oidc/JwtHandlerConfig.java
+++ openam-authentication/openam-auth-oidc/src/main/java/org/forgerock/openam/authentication/modules/oidc/JwtHandlerConfig.java
@@ -11,7 +11,7 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions copyright [year] [name of copyright owner]".
  *
- * Copyright 2014 ForgeRock AS.
+ * Copyright 2014-2016 ForgeRock AS.
  */
 
 package org.forgerock.openam.authentication.modules.oidc;
@@ -30,6 +30,7 @@ public class JwtHandlerConfig {
     static final String ISSUER_NAME_KEY = "openam-auth-openidconnect-issuer-name";
     static final String CRYPTO_CONTEXT_TYPE_KEY = "openam-auth-openidconnect-crypto-context-type";
     static final String CRYPTO_CONTEXT_VALUE_KEY = "openam-auth-openidconnect-crypto-context-value";
+    static final String KEY_CLIENT_SECRET = "iplanet-am-auth-oauth-client-secret";
 
     static final String CRYPTO_CONTEXT_TYPE_CONFIG_URL = ".well-known/openid-configuration_url";
     static final String CRYPTO_CONTEXT_TYPE_JWK_URL = "jwk_url";
@@ -38,12 +39,14 @@ public class JwtHandlerConfig {
     protected final String configuredIssuer;
     protected final String cryptoContextType;
     protected final String cryptoContextValue;
+    protected final String clientSecret;
     private final URL cryptoContextUrlValue;
 
     public JwtHandlerConfig(Map options) {
         configuredIssuer = CollectionHelper.getMapAttr(options, ISSUER_NAME_KEY);
         cryptoContextType = CollectionHelper.getMapAttr(options, CRYPTO_CONTEXT_TYPE_KEY);
         cryptoContextValue = CollectionHelper.getMapAttr(options, CRYPTO_CONTEXT_VALUE_KEY);
+        clientSecret = CollectionHelper.getMapAttr(options, KEY_CLIENT_SECRET);
         Reject.ifFalse(cryptoContextType == null || CRYPTO_CONTEXT_TYPE_CLIENT_SECRET.equals(cryptoContextType) ||
                 CRYPTO_CONTEXT_TYPE_JWK_URL.equals(cryptoContextType) ||
                 CRYPTO_CONTEXT_TYPE_CONFIG_URL.equals(cryptoContextType), "The value corresponding to key " +
@@ -77,4 +80,7 @@ public class JwtHandlerConfig {
         return cryptoContextUrlValue;
     }
 
+    public String getClientSecret() {
+        return clientSecret;
+    }
 }
