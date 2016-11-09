c810243ac626b096a7b795a7b37442b5086ab87f
Peter Major <peter.major@forgerock.com>
2016-11-09 21:14:51 +0000

    OPENAM-9425 Fix NPE in AMLoginContext

--- openam-core/src/main/java/com/sun/identity/authentication/service/AMLoginContext.java
+++ openam-core/src/main/java/com/sun/identity/authentication/service/AMLoginContext.java
@@ -1694,7 +1694,7 @@ public class AMLoginContext {
             // that in the query. otherwise it means a call with a new org.
             HttpServletRequest hreq = authContext.getLoginState().getHttpServletRequest();
             boolean isTokenValid = false;
-            final boolean isFederation = hreq.getAttribute(Constants.WSFED_ACTIVE_LOGIN) != null
+            final boolean isFederation = (hreq != null && hreq.getAttribute(Constants.WSFED_ACTIVE_LOGIN) != null)
                     || (indexType == IndexType.MODULE_INSTANCE && ISAuthConstants.FEDERATION_MODULE.equals(indexName));
             if (hreq != null && !isFederation) {
                 try {
