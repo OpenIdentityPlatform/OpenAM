3e14cfa95e7ba8a0bab81f2cacd78229bc10b7fa
maximthomas <maxim.thomas@gmail.com>
2019-03-11 10:28:46 +0300

    Mock HttpRequestContent for ESIA syncOffsetTest test

--- openam-authentication/openam-auth-oauth2/src/test/java/org/forgerock/openam/authentication/modules/oauth2/ESIATest.java
+++ openam-authentication/openam-auth-oauth2/src/test/java/org/forgerock/openam/authentication/modules/oauth2/ESIATest.java
@@ -1,15 +1,31 @@
 package org.forgerock.openam.authentication.modules.oauth2;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import java.net.URL;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import org.forgerock.openam.authentication.modules.oauth2.service.ESIAServiceUrlProvider;
 import org.forgerock.openam.authentication.modules.oauth2.service.esia.Signer;
+import org.mockito.Matchers;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.testng.PowerMockTestCase;
 import org.testng.annotations.Test;
 
-public class ESIATest {
+@PrepareForTest(HttpRequestContent.class)
+public class ESIATest extends PowerMockTestCase {
 
 	@Test
 	public void syncOffsetTest() throws Exception {
+		HttpRequestContent httpRequestContent =  mock(HttpRequestContent.class);
+		Map<String, List<String>> headers = Collections.singletonMap("Date", Collections.singletonList("Mon, 11 Mar 2019 07:07:25 GMT"));
+		when(httpRequestContent.getHeadersUsingHEAD(Matchers.anyString())).thenReturn(headers);
+		PowerMockito.mockStatic(HttpRequestContent.class);
+		PowerMockito.when(HttpRequestContent.getInstance()).thenReturn(httpRequestContent);
 		ESIAServiceUrlProvider.getSyncOffset();
 	}
 	
