1772e9b8e471f62e6d2629d39e7bb6651ac4219b
Maxim Thomas <maxim.thomas@gmail.com>
2019-07-15 11:58:49 +0300

    OpenAM Webauthn Authentication Module https://w3c.github.io/webauthn/ (#160)

---
+++ openam-authentication/openam-auth-webauthn/src/main/java/org/openidentityplatform/openam/authentication/modules/webauthn/WebAuthnAuthenticationProcessor.java
@@ -0,0 +1,125 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions Copyrighted [year] [name of copyright owner]".
+ *
+ * Copyright 2019 3A-Systems LLC. All rights reserved.
+ */
+
+package org.openidentityplatform.openam.authentication.modules.webauthn;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.ArrayUtils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sun.identity.authentication.spi.AuthLoginException;
+import com.webauthn4j.authenticator.Authenticator;
+import com.webauthn4j.data.PublicKeyCredentialDescriptor;
+import com.webauthn4j.data.PublicKeyCredentialRequestOptions;
+import com.webauthn4j.data.PublicKeyCredentialType;
+import com.webauthn4j.data.UserVerificationRequirement;
+import com.webauthn4j.data.WebAuthnAuthenticationContext;
+import com.webauthn4j.data.attestation.authenticator.AuthenticatorData;
+import com.webauthn4j.data.client.Origin;
+import com.webauthn4j.data.client.challenge.Challenge;
+import com.webauthn4j.data.client.challenge.DefaultChallenge;
+import com.webauthn4j.server.ServerProperty;
+import com.webauthn4j.validator.WebAuthnAuthenticationContextValidationResponse;
+import com.webauthn4j.validator.WebAuthnAuthenticationContextValidator;
+
+public class WebAuthnAuthenticationProcessor {
+	
+	private Challenge challenge;
+	
+	long timeout;
+	
+	public WebAuthnAuthenticationProcessor(String sessionId, long timeout) {
+		this.challenge = new DefaultChallenge(sessionId.getBytes());
+		this.timeout = timeout;
+	}
+	
+	public PublicKeyCredentialRequestOptions requestCredentials(String username, HttpServletRequest request, 
+			Set<Authenticator> authenticators) throws AuthLoginException, JsonProcessingException {
+		
+        String rpId = request.getServerName();
+
+        List<PublicKeyCredentialDescriptor> allowCredentials = new ArrayList<>();
+
+        for(Authenticator authenticator : authenticators) {
+            PublicKeyCredentialDescriptor publicKeyCredentialDescriptor = new PublicKeyCredentialDescriptor(
+                    PublicKeyCredentialType.PUBLIC_KEY,
+                    authenticator.getAttestedCredentialData().getCredentialId(),
+                    authenticator.getTransports()
+            );
+
+            allowCredentials.add(publicKeyCredentialDescriptor);
+        }
+        UserVerificationRequirement userVerification = UserVerificationRequirement.PREFERRED;
+
+        PublicKeyCredentialRequestOptions publicKeyCredentialRequestOptions = new PublicKeyCredentialRequestOptions(
+                challenge, timeout, rpId, allowCredentials, userVerification, null
+        );
+        
+        return publicKeyCredentialRequestOptions;
+	}
+	
+	public AuthenticatorData<?> processCredentials(HttpServletRequest request, String idStr, 
+			String authenticatorDataStr, String clientDataJSONStr, String signatureStr, 
+			String userHandleStr, Set<Authenticator> authenticators) {
+		byte[] id = Base64Utils.decodeFromUrlSafeString(idStr);
+		byte[] clientDataJSON = Base64Utils.decodeFromUrlSafeString(clientDataJSONStr);
+		byte[] authenticatorData = Base64Utils.decodeFromUrlSafeString(authenticatorDataStr);
+		byte[] signature = Base64Utils.decodeFromUrlSafeString(signatureStr);
+		
+		Authenticator foundAuthenticator = null;
+		for(Authenticator authenticator : authenticators ) {
+			if(ArrayUtils.isEquals(authenticator.getAttestedCredentialData().getCredentialId(), id)) {
+				foundAuthenticator = authenticator;
+				break;
+			}
+		}
+		
+		if(foundAuthenticator == null) {
+			return null;
+		}
+		
+		Origin origin = new Origin(request.getScheme(), request.getServerName(), request.getServerPort());
+        String rpId = request.getServerName();
+               
+        
+        byte[] tokenBindingId = null;
+        ServerProperty serverProperty = new ServerProperty(origin, rpId, challenge, tokenBindingId);
+        boolean userVerificationRequired = false;
+
+        WebAuthnAuthenticationContext authenticationContext =
+                new WebAuthnAuthenticationContext(
+                        id,
+                        clientDataJSON,
+                        authenticatorData,
+                        signature,
+                        serverProperty,
+                        userVerificationRequired
+                );
+        
+        WebAuthnAuthenticationContextValidator webAuthnAuthenticationContextValidator = 
+        		new WebAuthnAuthenticationContextValidator();
+
+        WebAuthnAuthenticationContextValidationResponse response = 
+        		webAuthnAuthenticationContextValidator.validate(authenticationContext, foundAuthenticator);
+        
+        return response.getAuthenticatorData();
+	}
+}
---
+++ openam-authentication/openam-auth-webauthn/src/test/java/org/openidentityplatform/openam/authentication/modules/webauthn/WebAuthnAuthenticationTest.java
@@ -0,0 +1,80 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions Copyrighted [year] [name of copyright owner]".
+ *
+ * Copyright 2019 3A-Systems LLC. All rights reserved.
+ */
+
+package org.openidentityplatform.openam.authentication.modules.webauthn;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+
+import java.util.Collections;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.NameCallback;
+import javax.servlet.http.HttpServletRequest;
+
+import org.mockito.Mockito;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.sun.identity.authentication.spi.AuthLoginException;
+import com.webauthn4j.authenticator.Authenticator;
+
+public class WebAuthnAuthenticationTest {
+
+	WebAuthnAuthentication webAuthnAuthentication;
+	
+	@BeforeMethod
+	public void initMocks() throws AuthLoginException {
+		
+		HttpServletRequest httpServletRequest =  mock(HttpServletRequest.class);
+		when(httpServletRequest.getScheme()).thenReturn("http");
+		when(httpServletRequest.getServerName()).thenReturn("localhost");
+		when(httpServletRequest.getServerPort()).thenReturn(8080);
+		
+		webAuthnAuthentication = mock(WebAuthnAuthentication.class, Mockito.CALLS_REAL_METHODS);
+		when(webAuthnAuthentication.getHttpServletRequest()).thenReturn(httpServletRequest);
+		when(webAuthnAuthentication.getSessionId()).thenReturn("87DCE7CF5F9DB00AC98367CA8640884F");
+		doNothing().when(webAuthnAuthentication).replaceCallback(anyInt(), anyInt(), any(Callback.class));
+		doReturn(Collections.<Authenticator>emptySet()).when(webAuthnAuthentication).loadAuthenticators();
+	}
+	
+	@Test
+	public void testProcessRequestUsername() throws Exception {
+		webAuthnAuthentication.init(null, Collections.EMPTY_MAP, Collections.EMPTY_MAP);
+		assertEquals(1, webAuthnAuthentication.process(null, 1));
+	}
+	
+	@Test
+	public void testProcessRequestCredentialsUserNameCallback() throws Exception {
+		webAuthnAuthentication.init(null, Collections.EMPTY_MAP, Collections.EMPTY_MAP);
+		NameCallback userNameCallback = new NameCallback("Username", "user1");
+		userNameCallback.setName("user1");
+		Callback[] callbacks = new Callback[] {userNameCallback};
+		assertEquals(webAuthnAuthentication.process(callbacks, 1), 2);
+	}
+	
+	@Test
+	public void testProcessRequestCredentials() throws Exception {
+		webAuthnAuthentication.init(null, Collections.singletonMap(webAuthnAuthentication.getUserKey(), "user1"), Collections.EMPTY_MAP);
+		assertEquals(webAuthnAuthentication.process(null, 1), 2);
+	}
+	
+}
