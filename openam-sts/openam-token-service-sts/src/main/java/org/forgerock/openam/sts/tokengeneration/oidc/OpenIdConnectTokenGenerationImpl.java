e054f7439bbb2adf22ab0d115d018b3af033565c
vharseko <vharseko@openam.org.ru>
2019-09-25 15:17:39 +0300

    auth:time:max getMaxSessionTime -> getTimeLeft

--- openam-sts/openam-token-service-sts/src/main/java/org/forgerock/openam/sts/tokengeneration/oidc/OpenIdConnectTokenGenerationImpl.java
+++ openam-sts/openam-token-service-sts/src/main/java/org/forgerock/openam/sts/tokengeneration/oidc/OpenIdConnectTokenGenerationImpl.java
@@ -173,7 +173,7 @@ public class OpenIdConnectTokenGenerationImpl implements OpenIdConnectTokenGener
 		        openIdConnectToken.put("auth:module", subjectToken.getProperty("AuthType", true));
 		        openIdConnectToken.put("auth:level", subjectToken.getProperty("AuthLevel", true));
 		        openIdConnectToken.put("auth:time:max:idle", currentTimeMillis()/1000+subjectToken.getMaxIdleTime()*60);
-		        openIdConnectToken.put("auth:time:max", currentTimeMillis()/1000+subjectToken.getMaxSessionTime()*60);
+		        openIdConnectToken.put("auth:time:max", currentTimeMillis()/1000+subjectToken.getTimeLeft()*60);
 	        }catch (SSOException e) {
 				throw new TokenCreationException(1,"token expired",e);
 			}
