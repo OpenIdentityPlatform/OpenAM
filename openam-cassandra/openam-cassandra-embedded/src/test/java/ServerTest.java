d8d3ff77c697ef697bb2ff3e172d853458bd9d53
vharseko <vharseko@3a-systems.ru>
2020-02-11 20:27:09 +0300

    reorganize commons build artifacts (#240)
    * reorganize commons build artifacts
    
    * remove com.sleepycat
    
    * update build deps

---
+++ openam-cassandra/openam-cassandra-embedded/src/test/java/ServerTest.java
@@ -0,0 +1,45 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2019 Open Identity Platform Community.
+ */
+
+
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.iplanet.sso.SSOException;
+import com.sun.identity.idm.IdRepoException;
+
+import org.openidentityplatform.openam.cassandra.embedded.Server;
+
+public class ServerTest {
+
+	static Server cassandra;
+	
+	@BeforeClass
+	public static void init() throws SSOException, IdRepoException{
+	}
+	
+	@AfterClass
+	public static void destory() throws SSOException, IdRepoException{
+	}
+	
+	@Test
+	public void start_test() throws SSOException, IdRepoException{
+		cassandra=new Server();
+		cassandra.run();
+	}
+}
