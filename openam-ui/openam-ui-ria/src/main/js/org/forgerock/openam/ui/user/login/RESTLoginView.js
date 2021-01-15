e32cea7ea5001c0dd34a9400bcdf94c2959def86
Valera V.Harseko <vharseko@3a-systems.ru>
2021-01-15 17:07:52 +0300

    FIX exceeds the maximum line length of 120  max-len

--- openam-ui/openam-ui-ria/src/main/js/org/forgerock/openam/ui/user/login/RESTLoginView.js
+++ openam-ui/openam-ui-ria/src/main/js/org/forgerock/openam/ui/user/login/RESTLoginView.js
@@ -56,11 +56,13 @@ define([
 
         // self-service links should be shown only on the first stage of the username/password stages
 
-        this.data.firstUserNamePassStage = Configuration.globalData.auth.currentStage === 1 && this.userNamePasswordStage;
+        this.data.firstUserNamePassStage = Configuration.globalData.auth.currentStage === 1 &&
+                                        this.userNamePasswordStage;
         this.data.showForgotPassword = this.userNamePasswordStage && Configuration.globalData.forgotPassword === "true";
         this.data.showForgotUserName = this.userNamePasswordStage && Configuration.globalData.forgotUsername === "true";
         this.data.showForgotten = this.data.showForgotPassword || this.data.showForgotUserName;
-        this.data.showSelfRegistration = this.userNamePasswordStage && Configuration.globalData.selfRegistration === "true";
+        this.data.showSelfRegistration = this.userNamePasswordStage &&
+                                        Configuration.globalData.selfRegistration === "true";
         this.data.showRememberLogin = this.userNamePasswordStage;
         // socialImplementations links should be shown only on the first stage of the username/password stages
         // and should not show on the upgrade session page
