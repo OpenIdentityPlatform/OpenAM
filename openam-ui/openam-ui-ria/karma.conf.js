97086645f0a2863d033536e10e83f6e1755ea5bc
Maxim Thomas <maxim.thomas@gmail.com>
2021-02-15 10:26:38 +0300

    revert logLevel for karma.conf.js

--- openam-ui/openam-ui-ria/karma.conf.js
+++ openam-ui/openam-ui-ria/karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function (config) {
         },
         port: 9876,
         colors: true,
-        logLevel: config.LOG_DEBUG,
+        logLevel: config.LOG_INFO,
         autoWatch: true,
         browsers: ["PhantomJS"],
         singleRun: false
