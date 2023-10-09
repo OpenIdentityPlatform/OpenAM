4494f915035ea3edac57bfcce90fb11615d606d7
Liu Woon Yung <pirorin187@gmail.com>
2023-10-09 17:05:22 +0800

    [#653] Fix NPE if attribute does not appear in schema, by skipping the attribute if it is not in the schema. (#654)
    Related to 281c1d71 (OPENAM-8616).

--- openam-core-rest/src/main/java/org/forgerock/openam/core/rest/sms/SmsJsonConverter.java
+++ openam-core-rest/src/main/java/org/forgerock/openam/core/rest/sms/SmsJsonConverter.java
@@ -393,7 +393,7 @@ public class SmsJsonConverter {
 
     private boolean shouldBeIgnored(String attributeName) {
         final AttributeSchema attributeSchema = schema.getAttributeSchema(attributeName);
-        return (attributeSchema != null && StringUtils.isBlank(attributeSchema.getI18NKey())) || attributeName.equals
+        return (attributeSchema == null || StringUtils.isBlank(attributeSchema.getI18NKey())) || attributeName.equals
                 ("_type") || hiddenAttributeNames.contains(attributeName);
     }
 
