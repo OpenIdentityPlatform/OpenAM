OpenIdentityPlatform/OpenAM

commit 9f368141da663b640c9defa6d40f9964436677ca

disable destroy current InternalSession + destory all sessions over limit

openam-core/src/main/java/com/iplanet/dpro/session/service/SessionConstraint.java
@@ -154,12 +154,8 @@ public class SessionConstraint {
             }
         }
 
-        if (sessions != null) {
-            sessionCount = sessions.size();
-        }
-
         // Step 3: checking the constraints
-        if (sessionCount >= quota) {
+        while (sessions != null && sessionCount >= sessions.size()) {
             // If the session quota internalSession exhausted, invoke the
             // pluggin to determine the desired behavior.
             reject = getQuotaExhaustionAction().action(internalSession, sessions);

openam-core/src/main/java/com/iplanet/dpro/session/service/InternalSession.java
@@ -740,11 +740,7 @@ public class InternalSession implements Serializable, AMSession, SessionPersiste
      * (2) the token is an application token (e.g. Agent)
      */
     private boolean shouldIgnoreSessionQuotaChecking() {
-        boolean ignore = false;
-        if (getState().equals(SessionState.VALID) || sessionService.isSuperUser(getUUID()) || (isAppSession())) {
-            ignore = true;
-        }
-        return ignore;
+        return (getState().equals(SessionState.VALID) || sessionService.isSuperUser(getUUID()) || (isAppSession()));
     }
 
     /**

openam-core/src/main/java/org/forgerock/openam/session/service/DestroyAllAction.java
@@ -36,6 +36,8 @@ import com.sun.identity.shared.debug.Debug;
 import java.util.Map;
 import java.util.Set;
 import javax.inject.Inject;
+
+import org.apache.commons.lang.StringUtils;
 import org.forgerock.guice.core.InjectorHolder;
 import org.forgerock.openam.session.SessionCache;
 
@@ -63,28 +65,24 @@ public class DestroyAllAction implements QuotaExhaustionAction {
 
     @Override
     public boolean action(InternalSession is, Map sessions) {
-        Set<String> sids = sessions.keySet();
+        final Set<String> sids = sessions.keySet();
         debug.message("there are " + sids.size() + " sessions");
-        synchronized (sessions) {
-            for (String sid : sids) {
-                SessionID sessID = new SessionID(sid);
-
-                try {
-                    Session s = sessionCache.getSession(sessID);
-                    s.destroySession(s);
-                    debug.message("Destroy sid " + sessID);
-                } catch (SessionException se) {
-                    if (debug.messageEnabled()) {
-                        debug.message("Failed to destroy the next "
-                                + "expiring session.", se);
-                    }
-
-                    // deny the session activation request
-                    // in this case
-                    return true;
-                }
-            }
-        }
+        for (String sid : sids) 
+        		if (!StringUtils.equals(is.getSessionID().toString(), sid))
+	        {
+	            final SessionID sessID = new SessionID(sid);
+	            try {
+	            		sessions.remove(sid);
+	                Session s = sessionCache.getSession(sessID);
+	                debug.warning("{} {} {} {}", this.getClass().getSimpleName(), sessID,s.getClientID(),s.getIdleTime());
+	                s.destroySession(s);
+	            } catch (SessionException se) {
+	                if (debug.messageEnabled()) {
+	                    debug.message("Failed to destroy the next "
+	                            + "expiring session.", se);
+	                }
+	            }
+	        }
         return false;
     }
 }

openam-core/src/main/java/org/forgerock/openam/session/service/DestroyNextExpiringAction.java
@@ -34,6 +34,8 @@ import com.iplanet.dpro.session.service.QuotaExhaustionAction;
 import com.sun.identity.shared.debug.Debug;
 import java.util.Map;
 import javax.inject.Inject;
+
+import org.apache.commons.lang.StringUtils;
 import org.forgerock.guice.core.InjectorHolder;
 import org.forgerock.openam.session.SessionCache;
 
@@ -56,30 +58,28 @@ public class DestroyNextExpiringAction implements QuotaExhaustionAction {
     public boolean action(InternalSession is, Map<String, Long> sessions) {
 
         String nextExpiringSessionID = null;
-        long smallestExpTime = Long.MAX_VALUE;
-        for (Map.Entry<String, Long> entry : sessions.entrySet()) {
-            String sid = entry.getKey();
-            long expirationTime = entry.getValue();
-            if (expirationTime < smallestExpTime) {
-                smallestExpTime = expirationTime;
-                nextExpiringSessionID = sid;
-            }
-
-        }
+        long largestExpTime = -1;
+        for (Map.Entry<String, Long> entry : sessions.entrySet()) 
+        		if (!StringUtils.equals(is.getSessionID().toString(), entry.getKey())){
+	            String sid = entry.getKey();
+	            long expirationTime = entry.getValue();
+	            if (expirationTime > largestExpTime) {
+	            		largestExpTime = expirationTime;
+	            		nextExpiringSessionID = sid;
+	            }
+	
+	        }
         if (nextExpiringSessionID != null) {
             SessionID sessID = new SessionID(nextExpiringSessionID);
             try {
+            		sessions.remove(nextExpiringSessionID);
                 Session s = sessionCache.getSession(sessID);
-                debug.warning("destroy {}", sessID);
+                debug.warning("{} {} {} from {} idle {}", this.getClass().getSimpleName(), sessID,s.getClientID(),sessions.size()+1,s.getIdleTime());
                 s.destroySession(s);
             } catch (SessionException e) {
                 if (debug.messageEnabled()) {
-                    debug.message("Failed to destroy the next "
-                            + "expiring session.", e);
+                    debug.message("Failed to destroy the next expiring session.", e);
                 }
-                // deny the session activation request
-                // in this case
-                return true;
             }
         }
         return false;

openam-core/src/main/java/org/forgerock/openam/session/service/DestroyOldestAction.java
@@ -32,6 +32,8 @@ import com.iplanet.dpro.session.service.QuotaExhaustionAction;
 import com.sun.identity.shared.debug.Debug;
 import java.util.Map;
 import javax.inject.Inject;
+
+import org.apache.commons.lang.StringUtils;
 import org.forgerock.guice.core.InjectorHolder;
 import org.forgerock.openam.session.SessionCache;
 
@@ -62,38 +64,35 @@ public class DestroyOldestAction implements QuotaExhaustionAction {
     public boolean action(InternalSession is, Map<String, Long> sessions) {
         long smallestExpTime = Long.MAX_VALUE;
         String oldestSessionID = null;
-        for (Map.Entry<String, Long> entry : sessions.entrySet()) {
-            try {
-                Session session = sessionCache.getSession(new SessionID(entry.getKey()));
-                session.refresh(false);
-                long expTime = session.getTimeLeft();
-                if (expTime < smallestExpTime) {
-                    smallestExpTime = expTime;
-                    oldestSessionID = entry.getKey();
-                }
-            } catch (SessionException ssoe) {
-                if (debug.warningEnabled()) {
-                    debug.warning("Failed to create SSOToken", ssoe);
-                }
-                // deny the session activation request
-                // in this case
-                return true;
-            }
-        }
+        for (Map.Entry<String, Long> entry : sessions.entrySet()) 
+	        	if (!StringUtils.equals(is.getSessionID().toString(), entry.getKey())){
+	        {
+	            try {
+	                Session session = sessionCache.getSession(new SessionID(entry.getKey()));
+	                session.refresh(false);
+	                long expTime = session.getTimeLeft();
+	                if (expTime <= smallestExpTime) {
+	                    smallestExpTime = expTime;
+	                    oldestSessionID = entry.getKey();
+	                }
+	            } catch (SessionException ssoe) {
+	                if (debug.warningEnabled()) {
+	                    debug.warning("Failed to create SSOToken", ssoe);
+	                }
+	            }
+	        }
 
         if (oldestSessionID != null) {
             SessionID sessID = new SessionID(oldestSessionID);
             try {
+            		sessions.remove(oldestSessionID);
                 Session s = sessionCache.getSession(sessID);
-                debug.warning("destroy {}", sessID);
+                debug.warning("{} {} {} from {} idle {}", this.getClass().getSimpleName(), sessID,s.getClientID(),sessions.size()+1,s.getIdleTime());
                 s.destroySession(s);
             } catch (SessionException e) {
                 if (debug.messageEnabled()) {
                     debug.message("Failed to destroy the next expiring session.", e);
                 }
-                // deny the session activation request
-                // in this case
-                return true;
             }
         }
         return false;

commit 332264377a6ae6e13ee63bef0969b107913346f8

Fix deadlock if iplanet-am-session-constraint-handler is set to DENY. This issue was introduced in 9f36814.

openam-core/src/main/java/com/iplanet/dpro/session/service/SessionConstraint.java
@@ -144,8 +144,11 @@ public class SessionConstraint {
          // Step 3: checking the constraints
             if (sessions != null && SystemProperties.getAsBoolean("org.openidentityplatform.openam.cts.quota.exhaustion.enabled", true)) {
             		sessions.remove(internalSession.getSessionID().toString());
-	    	        while (sessions.size() >= quota) 
+	    	        while (sessions.size() >= quota) {
 	    	            reject = getQuotaExhaustionAction().action(internalSession, sessions);
+	    	            if (reject)
+	    	                break;
+	    	        }
             }
         } catch (Exception e) {
             if (InjectorHolder.getInstance(SessionServiceConfig.class).isDenyLoginIfDBIsDown()) {