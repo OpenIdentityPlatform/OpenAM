5c13f83795355292e02f5ab0f6e9ab3724d3f296
Valera V Harseko <vharseko@3a-systems.ru>
2023-12-28 17:50:48 +0300

    CASSANDRA disable server tracing by default org.openidentityplatform.openam.cassandra.trace.server=false

--- openam-cassandra/openam-cassandra-datastore/src/main/java/org/openidentityplatform/openam/cassandra/ExecuteCallback.java
+++ openam-cassandra/openam-cassandra-datastore/src/main/java/org/openidentityplatform/openam/cassandra/ExecuteCallback.java
@@ -16,18 +16,25 @@
 
 package org.openidentityplatform.openam.cassandra;
 
+import java.util.ArrayList;
 import java.util.concurrent.CompletionStage;
+
+import javax.xml.rpc.holders.StringHolder;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.AsyncResultSet;
+import com.datastax.oss.driver.api.core.cql.ColumnDefinitions;
 import com.datastax.oss.driver.api.core.cql.ExecutionInfo;
 import com.datastax.oss.driver.api.core.cql.QueryTrace;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.SimpleStatement;
 import com.datastax.oss.driver.api.core.cql.Statement;
+import com.datastax.oss.driver.internal.core.cql.DefaultBatchStatement;
+import com.datastax.oss.driver.internal.core.cql.DefaultBoundStatement;
+import com.iplanet.am.util.SystemProperties;
 
 
 public class ExecuteCallback {
@@ -39,7 +46,7 @@ public class ExecuteCallback {
 	
 	public ExecuteCallback(String profile,CqlSession session,Statement<?>  statement){
 		this.session=session;
-		this.statement=statement.setExecutionProfileName(profile).setTracing(logger.isTraceEnabled());
+		this.statement=statement.setExecutionProfileName(profile).setTracing(logger.isTraceEnabled() && SystemProperties.getAsBoolean(ExecuteCallback.class.getPa.concat(".trace.server"), false));
 	}
 	
 	public ResultSet execute(){
@@ -74,18 +81,66 @@ public class ExecuteCallback {
 		if (logger.isTraceEnabled() && result.getTracingId()!=null) { 
 			final QueryTrace trace=result.getQueryTrace();
 			logger.trace("{}μs {} {}",trace.getDurationMicros(),trace.getParameters(),trace.getCoordinatorAddress());
+		}else if (logger.isTraceEnabled()){
+			logger.trace("{} {} ms {}: {} {}: {}"
+					,statement.getExecutionProfileName() 
+					,System.currentTimeMillis()-start
+					,getKeyspace()
+					,debugQuery(statement)
+					,statement.getConsistencyLevel()
+			);
 		}
 	}
 	
 	public void onFailure(Throwable t) {
-		logger.warn("{} {} ms {}: {} {} {}: {}"
+		logger.warn("{} {} ms {}: {} {}: {}"
 				,statement.getExecutionProfileName()
 				,System.currentTimeMillis()-start
-				,statement.getKeyspace()==null?session.getKeyspace():statement.getKeyspace()
-				,statement instanceof SimpleStatement ? ((SimpleStatement)statement).getQuery():statement
-				,statement instanceof SimpleStatement ? ((SimpleStatement)statement).getNamedValues():null
+				,getKeyspace()
+				,debugQuery(statement)
 				,statement.getConsistencyLevel()
 				,t.getMessage()
 		);
+	}
+	
+	String getKeyspace() {
+		return statement.getKeyspace()==null?session.getKeyspace().get().toString():statement.getKeyspace().toString();
+	}
+	
+	static ArrayList<String> debugQuery(Statement<?>  statement) {
+		final ArrayList<String> debugs=new ArrayList<String>();
+		if (statement instanceof DefaultBoundStatement) {
+			final StringHolder query=new StringHolder(((DefaultBoundStatement)statement).getPreparedStatement().getQuery());
+			final ColumnDefinitions cd=((DefaultBoundStatement)statement).getPreparedStatement().getVariableDefinitions();
+			cd.forEach(c -> {
+				try {
+					if (c.getType().asCql(false, false).equals("text") ) {
+						query.value=query.value.replace(":"+c.getName().asInternal(),"'"+((DefaultBoundStatement)statement).getString(c.getName())+"'");
+					}else if (c.getType().asCql(false, false).equals("int") ) {
+						query.value=query.value.replace(":"+c.getName().asInternal(),""+((DefaultBoundStatement)statement).getInt(c.getName()));
+					}else if (c.getType().asCql(false, false).equals("timestamp") ) {
+						query.value=query.value.replace(":"+c.getName().asInternal(),""+((DefaultBoundStatement)statement).getInstant(c.getName()).toString());
+					}else if (c.getType().asCql(false, false).equals("list<text>") ) {
+						query.value=query.value.replace(":"+c.getName().asInternal(),""+((DefaultBoundStatement)statement).getList(c.getName(),String.class));
+					}else {
+						query.value=query.value+" unknown type "+c.getType().asCql(false, false);
+					}
+				}catch (Throwable e) {
+					query.value=query.value+" error "+e.toString();
+				}
+			});
+			debugs.add(query.value);
+		}else if (statement instanceof SimpleStatement) {
+			debugs.add(((SimpleStatement)statement).getQuery()+" "+((SimpleStatement)statement).getNamedValues());
+		}else if (statement instanceof DefaultBatchStatement) {
+			((DefaultBatchStatement)statement).forEach(t -> {
+				debugs.addAll(debugQuery(t));
+				}
+			);
+			
+		}else {
+			debugs.add("unknown type: "+statement); 
+		}
+		return debugs;
 	}
 }
