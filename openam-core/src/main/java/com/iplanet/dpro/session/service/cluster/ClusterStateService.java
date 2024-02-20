df1e81dbe5db3909020589bbed8d21e3f3cfe2c1
maximthomas <maxim.thomas@gmail.com>
2024-02-20 11:48:41 +0300

    Add system property to disable cross-sites monitoring

--- openam-core/src/main/java/com/iplanet/dpro/session/service/cluster/ClusterStateService.java
+++ openam-core/src/main/java/com/iplanet/dpro/session/service/cluster/ClusterStateService.java
@@ -29,6 +29,7 @@
 /*
  * Portions Copyrighted 2010-2016 ForgeRock AS.
  * Portions Copyrighted 2014 Nomura Research Institute, Ltd.
+ * Portions Copyrighted 2023 3A Systems LLC.
  */
 
 package com.iplanet.dpro.session.service.cluster;
@@ -124,22 +125,22 @@ public class ClusterStateService extends GeneralTaskRunnable {
     /**
      * Service Globals
      */
-    public static Debug sessionDebug = null;
+    public static Debug sessionDebug;
 
     /**
      * Servers in the cluster environment
      */
-    private static Map<String, StateInfo> servers = new HashMap<String, StateInfo>();
+    private static final Map<String, StateInfo> servers = new HashMap<>();
 
     /**
      * Sites in the environment
      */
-    private static Map<String, StateInfo> sites = new HashMap<String, StateInfo>();
+    private static final Map<String, StateInfo> sites = new HashMap<>();
 
     /**
      * Sites and Servers that are currently down.
      */
-    private static Set<String> down = new HashSet<String>();
+    private static final Set<String> down = new HashSet<>();
 
     /**
      * Server Information
@@ -157,8 +158,11 @@ public class ClusterStateService extends GeneralTaskRunnable {
     public static final int DEFAULT_TIMEOUT = 1000;
 
     private static boolean doRequest = true;
+
     private static final String doRequestFlag = SystemProperties.
             get(Constants.URLCHECKER_DOREQUEST, "false");
+    private static final boolean MONITOR_CLUSTER_SITES = SystemProperties.
+            getAsBoolean("org.openidentityplatform.cluster.monitorSites", true);
 
     private int timeout = DEFAULT_TIMEOUT; // in milliseconds
 
@@ -248,14 +252,20 @@ public class ClusterStateService extends GeneralTaskRunnable {
             this.timeout = timeout;
             this.period = period;
 
-            serverSelectionList = new StateInfo[serverMembers.size() + siteMembers.size()];
+            if(MONITOR_CLUSTER_SITES) {
+                serverSelectionList = new StateInfo[serverMembers.size() + siteMembers.size()];
+            } else {
+                serverSelectionList = new StateInfo[serverMembers.size()];
+            }
 
             for (Map.Entry<String, String> entry : serverMembers.entrySet()) {
                 populateMap(servers, getServerInfo(entry.getKey(), entry.getValue()));
             }
 
-            for (Map.Entry<String, String> entry : siteMembers.entrySet()) {
-                populateMap(sites, getServerInfo(entry.getKey(), entry.getValue()));
+            if(MONITOR_CLUSTER_SITES) {
+                for (Map.Entry<String, String> entry : siteMembers.entrySet()) {
+                    populateMap(sites, getServerInfo(entry.getKey(), entry.getValue()));
+                }
             }
 
             // to ensure that ordering in different server instances is identical
@@ -477,7 +487,7 @@ public class ClusterStateService extends GeneralTaskRunnable {
             return true;
         }
 
-        boolean result = false;
+        boolean result;
         Socket sock = null;
         InputStream is = null;
 
@@ -500,13 +510,7 @@ public class ClusterStateService extends GeneralTaskRunnable {
                     connection.setReadTimeout(timeout);
 
                     if (connection instanceof HttpsURLConnection) {
-                        ((HttpsURLConnection) connection).setHostnameVerifier(new HostnameVerifier() {
-
-                            @Override
-                            public boolean verify(String hostname, SSLSession session) {
-                                return true;
-                            }
-                        });
+                        ((HttpsURLConnection) connection).setHostnameVerifier((hostname, session) -> true);
                     }
                     is = connection.getInputStream();
                     responseCode = connection.getResponseCode();
