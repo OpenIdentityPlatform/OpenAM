066a5ec364b2c8ceb5c82a974ebfa015d816e4c0
Maxim Thomas <maxim.thomas@gmail.com>
2024-11-26 15:37:18 +0300

    Update VK, OK OAuth 2.0 providers, added mail.ru OAuth 2.0 provider (#813)

---
+++ openam-authentication/openam-auth-oauth2/src/main/java/org/forgerock/openam/authentication/modules/oauth2/profile/MailRuProfileProvider.java
@@ -0,0 +1,41 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2024 3A Systems LLC.
+ */
+
+package org.forgerock.openam.authentication.modules.oauth2.profile;
+
+import org.forgerock.openam.authentication.modules.oauth2.HttpRequestContent;
+import org.forgerock.openam.authentication.modules.oauth2.OAuthConf;
+
+import javax.security.auth.login.LoginException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MailRuProfileProvider implements ProfileProvider {
+
+    private static final ProfileProvider INSTANCE = new MailRuProfileProvider();
+
+    public static ProfileProvider getInstance() {
+        return INSTANCE;
+    }
+    @Override
+    public String getProfile(OAuthConf config, String token) throws LoginException {
+        Map<String, String> profileServiceGetParameters = new HashMap<>();
+        profileServiceGetParameters.put("access_token", token);
+        profileServiceGetParameters.putAll(config.getProfileServiceGetParameters());
+        return HttpRequestContent.getInstance().getContentUsingGET(config.getProfileServiceUrl(), null,
+                profileServiceGetParameters);
+    }
+}
--- openam-authentication/openam-auth-oauth2/src/main/java/org/forgerock/openam/authentication/modules/oauth2/profile/OdnoklassnikiProvider.java
+++ openam-authentication/openam-auth-oauth2/src/main/java/org/forgerock/openam/authentication/modules/oauth2/profile/OdnoklassnikiProvider.java
@@ -1,3 +1,19 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Copyright 2018-2024 3A Systems LLC.
+ */
+
 package org.forgerock.openam.authentication.modules.oauth2.profile;
 
 import java.io.UnsupportedEncodingException;
@@ -39,7 +55,6 @@ public class OdnoklassnikiProvider implements ProfileProvider {
 		Map<String, String> parameters = new HashMap<>();
 		parameters.put("application_key", publicKey);
 		parameters.put("format", "JSON");
-		System.err.println(profileServiceUrl);
 		try {
 			signRequest(token, parameters, privateKey);
 		} catch(Exception e) { 
