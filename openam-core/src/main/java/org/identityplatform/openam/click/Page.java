4dca5c1cc4eeb6e80da3d69e0de9b2f2956767ed
maximthomas <maxim.thomas@gmail.com>
2025-01-29 16:50:03 +0300

    apache click to jakarta

---
+++ openam-core/src/main/java/org/identityplatform/openam/click/Page.java
@@ -0,0 +1,11 @@
+package org.identityplatform.openam.click;
+
+import org.apache.click.Context;
+
+public class Page extends org.apache.click.Page {
+
+    @Override
+    public Context getContext() {
+        return super.getContext();
+    }
+}
