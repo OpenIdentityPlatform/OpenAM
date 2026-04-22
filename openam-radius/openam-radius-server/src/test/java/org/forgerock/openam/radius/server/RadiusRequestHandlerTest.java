75ad75020f58dda20fdf27d5235e5963b9c5c14f
Valery Kharseko <vharseko@3a-systems.ru>
2026-04-22 14:27:21 +0300

    Update openam-radius/openam-radius-server/src/test/java/org/forgerock/openam/radius/server/RadiusRequestHandlerTest.java
    Co-authored-by: Maxim Thomas <maxim.thomas@gmail.com>

--- openam-radius/openam-radius-server/src/test/java/org/forgerock/openam/radius/server/RadiusRequestHandlerTest.java
+++ openam-radius/openam-radius-server/src/test/java/org/forgerock/openam/radius/server/RadiusRequestHandlerTest.java
@@ -12,8 +12,6 @@
  * information: "Portions copyright [year] [name of copyright owner]".
  *
  * Copyrighted 2015 Intellectual Reserve, Inc (IRI)
-
- * Portions copyright 2026 3A Systems, LLC.
  */
 /*
  * Portions copyright 2015 ForgeRock AS
